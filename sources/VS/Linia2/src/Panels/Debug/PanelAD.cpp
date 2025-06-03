b2562d84090a409670ed699c66affe5af6fcd414
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-06-03 14:03:53 +0300

    *

---
+++ sources/VS/Linia2/src/Panels/Debug/PanelAD.cpp
@@ -0,0 +1,3 @@
+﻿// 2025/6/3 14:01:53 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Panels/Debug/PanelAD.h"
---
+++ sources/VS/Linia2/src/Panels/Debug/PanelAD.h
@@ -0,0 +1,7 @@
+﻿// 2025/6/3 14:02:10 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+// Это панель для засылки данных в регистры микросхем типа AD
+
+
