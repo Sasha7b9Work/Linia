3bf9ca8c338037087f7f9f3617eeefa35b8b44e7
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-06-03 14:07:55 +0300

    *

---
+++ sources/VS/Linia2/src/Panels/Debug/NotebookDebug.cpp
@@ -0,0 +1,4 @@
+﻿// 2025/6/3 14:06:23 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Panels/Debug/NotebookDebug.h"
+
---
+++ sources/VS/Linia2/src/Panels/Debug/NotebookDebug.h
@@ -0,0 +1,2 @@
+﻿// 2025/6/3 14:06:45 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
