b7ee08feb355754792238107eabadf0b2e88ec86
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-06-04 16:45:48 +0300

    *

--- sources/VS/Linia2/src/Panels/Debug/PainterRegister.cpp
+++ sources/VS/Linia2/src/Panels/Debug/PainterRegister.cpp
@@ -1,6 +1,7 @@
 ﻿// 2025/6/4 15:50:26 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/Debug/PainterRegister.h"
+#include "Panels/Debug/PanelRegister.h"
 
 
 PainterRegister::PainterRegister(wxWindow *parent, PanelRegister *_panel, const wxPoint &position, const wxSize &size) :
@@ -20,6 +21,18 @@ void PainterRegister::OnPaint(wxPaintEvent &)
     gc->SetPen(*wxBLACK_PEN);
     gc->SetBrush(wxBrush(*wxWHITE));
     gc->DrawRectangle(0, 0, GetSize().x - 1, GetSize().y - 1);
+
+    int num_bits = panel->bit_depth;
+
+    int x0 = 36;
+    int y0 = 40;
+    int w = 20;
+    int h = w;
+
+    for (int i = 0; i < num_bits; i++)
+    {
+        gc->DrawRectangle(x0 + i * w, y0, w, h);
+    }
 
     delete gc;
 }
