e16ff471b423f9cfd650263dc51188cc63269ee6
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-06-21 23:37:45 +0300

    *

--- sources/VS/Linia2/src/Panels/Debug/PageMCP4811.cpp
+++ sources/VS/Linia2/src/Panels/Debug/PageMCP4811.cpp
@@ -9,4 +9,29 @@ PageMCP4811 *PageMCP4811::self = nullptr;
 PageMCP4811::PageMCP4811(wxNotebook *parent) : PageChip(parent, "MCP4811")
 {
     self = this;
+
+    wxArrayString names;
+
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            names.push_back("X");
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            names.push_back(wxString::Format("D%d", i));
+        }
+
+        names.push_back("SHDN");
+        names.push_back("GA");
+        names.push_back("-");
+        names.push_back("0");
+
+        PanelRegister *reg5_2 = new PanelRegister(this, "Register 5-2", 16, false, false);
+
+        reg5_2->SetNamesBits(names);
+
+        AppendRegister(reg5_2);
+    }
 }
