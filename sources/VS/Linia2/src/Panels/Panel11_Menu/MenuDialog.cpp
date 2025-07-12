fbb2e38c1bb974a9c88f1e0d8d595e0ad706bbe9
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-12 10:56:59 +0300

    refact

---
+++ sources/VS/Linia2/src/Panels/Panel11_Menu/MenuDialog.cpp
@@ -0,0 +1,3 @@
+﻿// 2025/7/12 10:56:03 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Panels/Panel11_Menu/MenuDialog.h"
---
+++ sources/VS/Linia2/src/Panels/Panel11_Menu/MenuDialog.h
@@ -0,0 +1,2 @@
+﻿// 2025/7/12 10:56:27 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
