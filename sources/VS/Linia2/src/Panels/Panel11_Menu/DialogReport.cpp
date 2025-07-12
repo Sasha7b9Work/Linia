2212c1661eefea60eaf20d1c0059d3dfe0aba981
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-12 19:10:31 +0300

    *

--- sources/VS/Linia2/src/Panels/Panel11_Menu/DialogReport.cpp
+++ sources/VS/Linia2/src/Panels/Panel11_Menu/DialogReport.cpp
@@ -1,4 +1,4 @@
-// 2025/7/12 18:36:24 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+п»ї// 2025/7/12 18:36:24 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/Panel11_Menu/DialogReport.h"
 
@@ -7,13 +7,13 @@ DialogReport *DialogReport::self = nullptr;
 
 
 DialogReport::DialogReport() :
-    MenuDialog(_("Отчёт"), 100,
-        _L("Новый"), ID_BTN_MENU_REPORT_NEW, OnButtonNew,
-        _L("Сохранить"), ID_BTN_MENU_REPORT_SAVE, OnButtonSave,
-        _L("Удалить"), ID_BTN_MENU_REPORT_DELETE, OnButtonDelete,
-        _L("Развернуть"), ID_BTN_MENU_REPORT_EXPAND, OnButtonExpand,
-        _L("Свернуть"), ID_BTN_MENU_REPORT_COLLAPSE, OnButtonCollapse,
-        _L("Закрыть"), ID_BTN_MENU_REPORT_CLOSE, OnButtonClose
+    MenuDialog(_("РћС‚С‡С‘С‚"), 100,
+        _L("РќРѕРІС‹Р№"), ID_BTN_MENU_REPORT_NEW, OnButtonNew,
+        _L("РЎРѕС…СЂР°РЅРёС‚СЊ"), ID_BTN_MENU_REPORT_SAVE, OnButtonSave,
+        _L("РЈРґР°Р»РёС‚СЊ"), ID_BTN_MENU_REPORT_DELETE, OnButtonDelete,
+        _L("Р Р°Р·РІРµСЂРЅСѓС‚СЊ"), ID_BTN_MENU_REPORT_EXPAND, OnButtonExpand,
+        _L("РЎРІРµСЂРЅСѓС‚СЊ"), ID_BTN_MENU_REPORT_COLLAPSE, OnButtonCollapse,
+        _L("Р—Р°РєСЂС‹С‚СЊ"), ID_BTN_MENU_REPORT_CLOSE, OnButtonClose
     )
 {
     self = this;
