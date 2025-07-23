1234c8290139d13bf2442e201e2ad8c52b17df58
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-23 22:42:14 +0300

    *

--- sources/VS/Linia2/src/Panels/Panel04_Model.cpp
+++ sources/VS/Linia2/src/Panels/Panel04_Model.cpp
@@ -1,14 +1,32 @@
 // 2025/6/1 17:41:03 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/Panel04_Model.h"
-#include "MainWindow.h"
+#include "Utils/GlobalFunctions.h"
 
 
 PanelModel *PanelModel::self = nullptr;
 
 
 PanelModel::PanelModel(wxWindow* parent) :
-    Panel(parent, 0, MainWindow::HEIGTH1, MainWindow::WIDTH1, HEIGHT)
+    Panel(parent, 0, MainWindow::HEIGTH1, WIDTH, HEIGHT)
 {
     self = this;
+
+    const int height_name = 20;
+
+    wxPanel *panel_name = new wxPanel(this, wxID_ANY, { 0, 0 }, { WIDTH, height_name }, wxTAB_TRAVERSAL | wxSUNKEN_BORDER);
+
+    new wxStaticText(panel_name, ID_PAN4_TEXT_NAME, "", { 0, 0 }, { WIDTH, height_name }, wxALIGN_CENTER);
+
+    SetName(_L("Файл модели"));
+
+    panel_graph = new wxPanel(this, wxID_ANY, { 0, height_name }, { WIDTH, HEIGHT - height_name }, wxTAB_TRAVERSAL | wxSUNKEN_BORDER);
+
+    panel_graph->SetBackgroundColour(*wxWHITE);
+}
+
+
+void PanelModel::SetName(const wxString &name)
+{
+    GF::FindStaticText(this, ID_PAN4_TEXT_NAME)->SetLabel(name);
 }
--- sources/VS/Linia2/src/Panels/Panel04_Model.h
+++ sources/VS/Linia2/src/Panels/Panel04_Model.h
@@ -1,15 +1,23 @@
 // 2025/6/1 17:41:14 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 #include "Panels/Panel.h"
+#include "MainWindow.h"
 
 
 class PanelModel : public Panel
 {
 public:
 
+    static const int WIDTH = MainWindow::WIDTH1;
     static const int HEIGHT = 400;
 
     PanelModel(wxWindow *parent);
 
     static PanelModel *self;
+
+private:
+
+    wxPanel *panel_graph = nullptr;
+
+    void SetName(const wxString &);
 };
