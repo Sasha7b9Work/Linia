4d30515a807aa8295bfd0b85139ad7542b4f35f2
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-24 15:32:00 +0300

    *

--- sources/VS/Linia2/src/Panels/Panel08_ModeLower.cpp
+++ sources/VS/Linia2/src/Panels/Panel08_ModeLower.cpp
@@ -1,4 +1,4 @@
-// 2025/6/1 18:03:13 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+п»ї// 2025/6/1 18:03:13 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/Panel04_Model.h"
 #include "Panels/Panel06_Percents.h"
@@ -14,17 +14,17 @@ PanelModeLower::PanelModeLower(wxWindow *parent) :
 {
     self = this;
 
-    wxStaticBox *boxCategory = new wxStaticBox(this, wxID_ANY, _L("Режим"), { 5, 0 }, { WIDTH - 13, HEIGHT - 8});
+    wxStaticBox *boxCategory = new wxStaticBox(this, wxID_ANY, _L("Р РµР¶РёРј"), { 5, 0 }, { WIDTH - 13, HEIGHT - 8});
 
     {
-        new wxStaticText(boxCategory, wxID_ANY, _L("Канал") + " C:", { 10, SD::Y_SB(30) });
+        new wxStaticText(boxCategory, wxID_ANY, _L("РљР°РЅР°Р»") + " C:", { 10, SD::Y_SB(30) });
 
-        new wxStaticText(boxCategory, wxID_ANY, _L("Канал") + " B:", { 10, SD::Y_SB(60) });
+        new wxStaticText(boxCategory, wxID_ANY, _L("РљР°РЅР°Р»") + " B:", { 10, SD::Y_SB(60) });
 
-        new wxStaticText(boxCategory, wxID_ANY, wxString("1 ") + _L("очередь"), { 5, SD::Y_SB(100) });
+        new wxStaticText(boxCategory, wxID_ANY, wxString("1 ") + _L("РѕС‡РµСЂРµРґСЊ"), { 5, SD::Y_SB(100) });
 
         wxArrayString choices;
-        choices.Add(_L("Канал") + " C");
+        choices.Add(_L("РљР°РЅР°Р»") + " C");
 
         new wxComboBox(boxCategory, ID_PAN8_COMBO, choices[0], { 5, 120 }, { 80, TEXTCNTRL_HEIGHT }, choices, wxCB_READONLY);
     }
