d415c67e60d3da086c188f47207f7ec3d1e28579
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-27 14:52:54 +0300

    refact

--- sources/VS/Linia2/src/Panels/Panel11_Menu/DialogTable.cpp
+++ sources/VS/Linia2/src/Panels/Panel11_Menu/DialogTable.cpp
@@ -3,15 +3,24 @@
 #include "Panels/Panel11_Menu/DialogTable.h"
 
 
+enum
+{
+    ID_MENU_SAVE = wxID_HIGHEST + 1,
+    ID_MENU_DELETE,
+    ID_MENU_EXPAND,
+    ID_MENU_COLLAPSE
+};
+
+
 DialogTable *DialogTable::self = nullptr;
 
 
 DialogTable::DialogTable() :
     MenuDialog(_L("Таблица"), 100,
-        _L("Сохранить"), ID_BTN_MENU_TABLE_SAVE, OnButtonSave,
-        _L("Удалить"), ID_BTN_MENU_TABLE_DELETE, OnButtonDelete,
-        _L("Развернуть"), ID_BTN_MENU_TABLE_EXPAND, OnButtonExpand,
-        _L("Свернуть"), ID_BTN_MENU_TABLE_COLLAPSE, OnButtonCollapse
+        _L("Сохранить"), ID_MENU_SAVE, OnButtonSave,
+        _L("Удалить"), ID_MENU_DELETE, OnButtonDelete,
+        _L("Развернуть"), ID_MENU_EXPAND, OnButtonExpand,
+        _L("Свернуть"), ID_MENU_COLLAPSE, OnButtonCollapse
     )
 {
     self = this;
