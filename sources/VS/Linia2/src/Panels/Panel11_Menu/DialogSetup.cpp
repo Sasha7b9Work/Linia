1a6885b632521867ac933b1bf5032be357be8184
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-27 14:48:02 +0300

    refact

--- sources/VS/Linia2/src/Panels/Panel11_Menu/DialogSetup.cpp
+++ sources/VS/Linia2/src/Panels/Panel11_Menu/DialogSetup.cpp
@@ -4,20 +4,34 @@
 #include "Panels/Panel11_Menu/WindowCorrectionZero.h"
 
 
+enum
+{
+    ID_BTN_USER = wxID_HIGHEST + 1,
+    ID_BTN_START,
+    ID_BTN_AUTOSAVE,
+    ID_BTN_CORRECTION_ZERO,
+    ID_BTN_CALIBRATION,
+    ID_BTN_TABLE,
+    ID_BTN_PASSWORD,
+    ID_BTN_ADDRESS_IP,
+    ID_BTN_MY_COMPUTER
+};
+
+
 DialogSetup *DialogSetup::self = nullptr;
 
 
 DialogSetup::DialogSetup() :
     MenuDialog(_L("Настройка"), 275,
-        _L("Настройки пользователя"), ID_BTN_MENU_SETUP_USER, OnButtonUser,
-        _L("Запуск"), ID_BTN_MENU_SETUP_START, OnButtonStart,
-        _L("Автосохранение"), ID_BTN_MENU_SETUP_AUTOSAVE, OnButtonAutoSave,
-        _L("Коррекция смещения нуля"), ID_BTN_MENU_SETUP_CORRECTION_ZERO, OnButtonCorrectionZero,
-        _L("Калибровка"), ID_BTN_MENU_SETUP_CALIBRATION, OnButtonCalibration,
-        _L("Таблица смещений и коэффициентов"), ID_BTN_MENU_SETUP_TABLE, OnButtonTable,
-        _L("Изменить пароль"), ID_BTN_MENU_SETUP_PASSWORD, OnButtonPassword,
-        _L("IP-адрес"), ID_BTN_MENU_SETUP_ADDRESS_IP, OnButtonAddressIP,
-        _L("Мой компьютер"), ID_BTN_MENU_SETUP_MY_COMPUTER, OnButtonMyComputer
+        _L("Настройки пользователя"), ID_BTN_USER, OnButtonUser,
+        _L("Запуск"), ID_BTN_START, OnButtonStart,
+        _L("Автосохранение"), ID_BTN_AUTOSAVE, OnButtonAutoSave,
+        _L("Коррекция смещения нуля"), ID_BTN_CORRECTION_ZERO, OnButtonCorrectionZero,
+        _L("Калибровка"), ID_BTN_CALIBRATION, OnButtonCalibration,
+        _L("Таблица смещений и коэффициентов"), ID_BTN_TABLE, OnButtonTable,
+        _L("Изменить пароль"), ID_BTN_PASSWORD, OnButtonPassword,
+        _L("IP-адрес"), ID_BTN_ADDRESS_IP, OnButtonAddressIP,
+        _L("Мой компьютер"), ID_BTN_MY_COMPUTER, OnButtonMyComputer
     )
 {
     self = this;
