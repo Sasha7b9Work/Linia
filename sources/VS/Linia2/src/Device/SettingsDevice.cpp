103366847d9ba737ed7c20b505c232376a9334b3
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-07-27 22:34:49 +0300

    *

---
+++ sources/VS/Linia2/src/Device/SettingsDevice.cpp
@@ -0,0 +1,20 @@
+﻿// 2025/7/27 22:27:36 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Device/SettingsDevice.h"
+
+
+pchar RangeI::Name() const
+{
+    static const pchar names[Count] =
+    {
+        "1nA",
+        "2nA",
+        "5nA",
+        "10nA",
+        "20nA",
+        "50na",
+        "100nA",
+    };
+
+    return names[value];
+}
--- sources/VS/Linia2/src/Device/SettingsDevice.h
+++ sources/VS/Linia2/src/Device/SettingsDevice.h
@@ -1,2 +1,85 @@
 ﻿// 2025/7/27 22:17:39 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
+
+
+struct RangeI
+{
+    enum E
+    {
+        _1nA,
+        _2nA,
+        _5nA,
+        _10nA,
+        _20nA,
+        _50nA,
+        _100nA,
+        _200nA,
+        _500nA,
+        _1uA,
+        _2uA,
+        _5uA,
+        _10uA,
+        _20uA,
+        _50uA,
+        _100uA,
+        _200uA,
+        _500uA,
+        _1mA,
+        _2mA,
+        _5mA,
+        _10mA,
+        _20mA,
+        _50mA,
+        _100mA,
+        _200mA,
+        _500mA,
+        _1A,
+        _2A,
+        _5A,
+        _10A,
+        _20A,
+        _50A,
+        _100A,
+        _200A,
+        _500A,
+        Count
+    };
+
+    E value;
+
+    RangeI(E v) : value(v) { }
+
+    pchar Name() const;
+};
+
+
+struct RangeU
+{
+    enum E
+    {
+        _50mV,
+        _100mV,
+        _200mV,
+        _500mV,
+        _1V,
+        _2V,
+        _5V,
+        _10V,
+        _20V,
+        _50V,
+        _100V,
+        _200V,
+        _500V,
+        _1kV,
+        _2kV,
+        _5kV,
+        _10kV,
+        Count
+    };
+
+    E value;
+
+    RangeU(E v) : value(v) { }
+
+    pchar Name() const;
+};
