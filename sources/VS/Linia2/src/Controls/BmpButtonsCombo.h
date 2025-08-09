ae71cd5b24d7082b4439bab32d63c3d4cd0996ec
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-09 22:44:09 +0300

    *

--- sources/VS/Linia2/src/Controls/BmpButtonsCombo.h
+++ sources/VS/Linia2/src/Controls/BmpButtonsCombo.h
@@ -23,6 +23,8 @@ private:
         ButtonPopup(wxWindow *parent, const wxString &title, const wxArrayString &files, const wxArrayString &tooltips, int buttons_in_row) :
             wxPopupTransientWindow(parent, wxBORDER_SUNKEN)
         {
+            SetWindowStyle(wxBORDER_SIMPLE | wxPU_CONTAINS_CONTROLS);
+
             // Основной контейнер с отступами по краям
             wxBoxSizer *outerSizer = new wxBoxSizer(wxVERTICAL);
             wxPanel *mainPanel = new wxPanel(this, wxID_ANY);
