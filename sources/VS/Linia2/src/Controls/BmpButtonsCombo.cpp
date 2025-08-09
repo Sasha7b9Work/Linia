b6a7406d5df39a494242236402121f5e5dfd0ffa
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-09 22:35:09 +0300

    Теперь всплывающее окно на кнопки с рисунками создаётся один раз

--- sources/VS/Linia2/src/Controls/BmpButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/BmpButtonsCombo.cpp
@@ -3,85 +3,24 @@
 #include "Controls/BmpButtonsCombo.h"
 
 
-class ButtonPopup : public wxPopupTransientWindow
-{
-public:
-    ButtonPopup(wxWindow *parent, const wxString &title, const wxArrayString &files, const wxArrayString &tooltips, int buttons_in_row) :
-        wxPopupTransientWindow(parent, wxBORDER_SUNKEN)
-    {
-        // Основной контейнер с отступами по краям
-        wxBoxSizer *outerSizer = new wxBoxSizer(wxVERTICAL);
-        wxPanel *mainPanel = new wxPanel(this, wxID_ANY);
-        mainPanel->SetBackgroundColour(*wxWHITE);
-
-        int num_rows = (int)(files.size() / buttons_in_row);
-
-        if (num_rows * buttons_in_row < (int)files.size())
-        {
-            num_rows++;
-        }
-
-        int num_cols = (int)(files.size() / num_rows);
-
-        if (num_rows * num_cols < (int)files.size())
-        {
-            num_cols++;
-        }
-
-        wxGridSizer *gridSizer = new wxGridSizer(num_rows, num_cols, 5, 5); // 5px промежутки
-
-        for (uint i = 0; i < files.size(); ++i)
-        {
-            ButtonBitmap *btn = new ButtonBitmap(mainPanel, wxDefaultPosition, wxDefaultSize, files[i]);
-            btn->SetToolTip(tooltips[i]);
-            btn->Bind(wxEVT_BUTTON, &ButtonPopup::OnButtonClick, this);
-            gridSizer->Add(btn, 0, wxEXPAND | wxALL, 2); // 2px отступы у кнопок
-        }
-
-        // Добавляем рамку вокруг сетки кнопок
-        wxStaticBoxSizer *boxSizer = new wxStaticBoxSizer(wxVERTICAL, mainPanel, title);
-        boxSizer->Add(gridSizer, 1, wxEXPAND | wxALL, 0); // 10px отступ внутри рамки
-
-        // Основная панель
-        mainPanel->SetSizer(boxSizer);
-
-        // Внешние отступы 15px
-        outerSizer->Add(mainPanel, 1, wxEXPAND | wxALL, 5);
-        SetSizer(outerSizer);
-
-        Fit(); // Автоподбор размера
-    }
-
-private:
-    void OnButtonClick(wxCommandEvent &)
-    {
-        Dismiss();
-    }
-};
-
-
-BmpButtonsCombo::BmpButtonsCombo(wxWindow *parent, const wxString &_title, const wxPoint &pos, const wxSize &, const wxArrayString &_files, const wxArrayString &_tooltips, int num_file, int _buttons_in_row) :
-    ButtonBitmap(parent, pos, wxDefaultSize, _files[(size_t)num_file])
+BmpButtonsCombo::BmpButtonsCombo(wxWindow *parent, const wxString &title, const wxPoint &pos, const wxSize &, const wxArrayString &files, const wxArrayString &tooltips, int num_file, int buttons_in_row) :
+    ButtonBitmap(parent, pos, wxDefaultSize, files[(size_t)num_file])
 {
     Bind(wxEVT_BUTTON, &BmpButtonsCombo::OnButtonClicked, this);
 
-    SetToolTip(_tooltips[(size_t)num_file]);
+    SetToolTip(tooltips[(size_t)num_file]);
 
-    title = _title;
-    files = _files;
-    tooltips = _tooltips;
-    buttons_in_row = _buttons_in_row;
+    popup = new ButtonPopup(this, title, files, tooltips, buttons_in_row);
+
+    popup->Hide();
 }
 
 
 void BmpButtonsCombo::OnButtonClicked(wxCommandEvent &)
 {
-    ButtonPopup *popup = new ButtonPopup(this, title, files, tooltips, buttons_in_row);
-
     wxPoint pos = ClientToScreen(wxPoint(GetSize().x / 2, GetSize().y / 2));
     pos.x -= popup->GetSize().x / 2;
     pos.y -= popup->GetSize().y / 2;
 
-    popup->Position(pos, wxSize(0, 0));
-    popup->Popup();
+    popup->ShowPopup(pos);
 }
--- sources/VS/Linia2/src/Controls/BmpButtonsCombo.h
+++ sources/VS/Linia2/src/Controls/BmpButtonsCombo.h
@@ -17,10 +17,77 @@ public:
 
 private:
 
-    wxString title;
-    wxArrayString files;
-    wxArrayString tooltips;
-    int buttons_in_row;
+    class ButtonPopup : public wxPopupTransientWindow
+    {
+    public:
+        ButtonPopup(wxWindow *parent, const wxString &title, const wxArrayString &files, const wxArrayString &tooltips, int buttons_in_row) :
+            wxPopupTransientWindow(parent, wxBORDER_SUNKEN)
+        {
+            // Основной контейнер с отступами по краям
+            wxBoxSizer *outerSizer = new wxBoxSizer(wxVERTICAL);
+            wxPanel *mainPanel = new wxPanel(this, wxID_ANY);
+            mainPanel->SetBackgroundColour(*wxWHITE);
+
+            int num_rows = (int)(files.size() / buttons_in_row);
+
+            if (num_rows * buttons_in_row < (int)files.size())
+            {
+                num_rows++;
+            }
+
+            int num_cols = (int)(files.size() / num_rows);
+
+            if (num_rows * num_cols < (int)files.size())
+            {
+                num_cols++;
+            }
+
+            wxGridSizer *gridSizer = new wxGridSizer(num_rows, num_cols, 5, 5); // 5px промежутки
+
+            for (uint i = 0; i < files.size(); ++i)
+            {
+                ButtonBitmap *btn = new ButtonBitmap(mainPanel, wxDefaultPosition, wxDefaultSize, files[i]);
+                btn->SetToolTip(tooltips[i]);
+                btn->Bind(wxEVT_BUTTON, &ButtonPopup::OnButtonClick, this);
+                gridSizer->Add(btn, 0, wxEXPAND | wxALL, 2); // 2px отступы у кнопок
+            }
+
+            // Добавляем рамку вокруг сетки кнопок
+            wxStaticBoxSizer *boxSizer = new wxStaticBoxSizer(wxVERTICAL, mainPanel, title);
+            boxSizer->Add(gridSizer, 1, wxEXPAND | wxALL, 0); // 10px отступ внутри рамки
+
+            // Основная панель
+            mainPanel->SetSizer(boxSizer);
+
+            // Внешние отступы 15px
+            outerSizer->Add(mainPanel, 1, wxEXPAND | wxALL, 5);
+            SetSizer(outerSizer);
+
+            Fit(); // Автоподбор размера
+        }
+
+        // Новая функция для показа
+        void ShowPopup(const wxPoint &pos)
+        {
+            Position(pos, wxSize(0, 0));
+            Show();
+            Popup(); // Активируем режим popup
+        }
+
+    private:
+
+        virtual void OnDismiss() override
+        {
+            Hide();
+        }
+
+        void OnButtonClick(wxCommandEvent &)
+        {
+            Hide();
+        }
+    };
+
+    ButtonPopup *popup = nullptr;
 
     void OnButtonClicked(wxCommandEvent &);
 };
