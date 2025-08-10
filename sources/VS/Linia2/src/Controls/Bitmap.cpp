01ead4866e5620bbc4d3d8715c40b89a166659de
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-10 13:50:14 +0300

    *

---
+++ sources/VS/Linia2/src/Controls/Bitmap.cpp
@@ -0,0 +1,48 @@
+﻿// 2025/08/10 13:41:02 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Controls/Bitmap.h"
+
+
+std::map<wxString, Bitmap> Bitmap::bitmaps;
+
+
+Bitmap::Bitmap(const wxString &file_bitmap)
+{
+    wxString path = wxString("resources/") + file_bitmap;
+
+    if (file_bitmap[file_bitmap.size() - 1] == 'p')
+    {
+        wxImage image;
+
+        if (!image.LoadFile(path, wxBITMAP_TYPE_BMP))
+        {
+            LOG_ERROR("Не удалось загрузить файл изображения %s", file_bitmap.c_str().AsChar());
+        }
+
+        bitmap = wxBitmap(image);
+
+        bitmap.SetMask(new wxMask(bitmap, *wxWHITE));
+    }
+    else
+    {
+        wxIcon icon;
+
+        if (!icon.LoadFile(path, wxBITMAP_TYPE_ICO))
+        {
+            LOG_ERROR("Не удалось загрузить файл изображения %s", file_bitmap.c_str().AsChar());
+        }
+
+        bitmap.CopyFromIcon(icon);
+    }
+}
+
+
+Bitmap Bitmap::Get(const wxString &file_name)
+{
+    if (bitmaps.find(file_name) == bitmaps.end())
+    {
+        bitmaps[file_name] = Bitmap(file_name);
+    }
+
+    return bitmaps[file_name];
+}
---
+++ sources/VS/Linia2/src/Controls/Bitmap.h
@@ -0,0 +1,20 @@
+﻿// 2025/08/10 13:41:12 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class Bitmap
+{
+public:
+
+    Bitmap() { }
+
+    Bitmap(const wxString &file_name);
+
+    static Bitmap Get(const wxString &file_name);
+
+private:
+
+    wxBitmap bitmap;
+
+    static std::map<wxString, Bitmap> bitmaps;
+};
