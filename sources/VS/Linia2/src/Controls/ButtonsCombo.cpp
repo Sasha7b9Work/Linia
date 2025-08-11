bf3f9bc292d0cb4083895add558271c871e03e8b
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-11 16:40:07 +0300

    Добавлено ButtonsCombo

--- sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
@@ -1,12 +1,12 @@
 ﻿// 2025/8/9 11:41:08 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "Controls/BmpButtonsCombo.h"
+#include "Controls/ButtonsCombo.h"
 
 
-class BmpButtonPopup : public wxPopupTransientWindow
+class ButtonPopup : public wxPopupTransientWindow
 {
 public:
-    BmpButtonPopup(wxWindow *parent, const wxString &title, const wxArrayString &files, const wxArrayString &tooltips, int buttons_in_row) :
+    ButtonPopup(wxWindow *parent, const wxString &title, const wxArrayString &names, const wxArrayString &tooltips, int buttons_in_row) :
         wxPopupTransientWindow(parent, wxBORDER_SUNKEN)
     {
         // Основной контейнер с отступами по краям
@@ -14,28 +14,28 @@ public:
         wxPanel *mainPanel = new wxPanel(this, wxID_ANY);
         mainPanel->SetBackgroundColour(*wxWHITE);
 
-        int num_rows = (int)(files.size() / buttons_in_row);
+        int num_rows = (int)(names.size() / buttons_in_row);
 
-        if (num_rows * buttons_in_row < (int)files.size())
+        if (num_rows * buttons_in_row < (int)names.size())
         {
             num_rows++;
         }
 
-        int num_cols = (int)(files.size() / num_rows);
+        int num_cols = (int)(names.size() / num_rows);
 
-        if (num_rows * num_cols < (int)files.size())
+        if (num_rows * num_cols < (int)names.size())
         {
             num_cols++;
         }
 
         wxGridSizer *gridSizer = new wxGridSizer(num_rows, num_cols, 5, 5); // 5px промежутки
 
-        for (uint i = 0; i < files.size(); ++i)
+        for (uint i = 0; i < names.size(); ++i)
         {
-            ButtonBitmap *btn = new ButtonBitmap(mainPanel, wxDefaultPosition, wxDefaultSize, files[i]);
+            wxButton *btn = new wxButton(mainPanel, wxID_ANY, names[i]);
             btn->SetLabel(wxString::Format("%d", i));
             btn->SetToolTip(tooltips[i]);
-            btn->Bind(wxEVT_BUTTON, &BmpButtonPopup::OnButtonClick, this);
+            btn->Bind(wxEVT_BUTTON, &ButtonPopup::OnButtonClick, this);
             gridSizer->Add(btn, 0, wxEXPAND | wxALL, 2); // 2px отступы у кнопок
         }
 
@@ -72,24 +72,24 @@ private:
 };
 
 
-BmpButtonsCombo::BmpButtonsCombo(wxWindow *parent, const wxString &_title, const wxPoint &pos, const wxSize &, const wxArrayString &_files, const wxArrayString &_tooltips, int num_file, int _buttons_in_row) :
-    ButtonBitmap(parent, pos, wxDefaultSize, _files[(size_t)num_file]),
-    current_choice(num_file)
+ButtonsCombo::ButtonsCombo(wxWindow *parent, const wxString &_title, const wxPoint &pos, const wxSize &, const wxArrayString &_names, const wxArrayString &_tooltips, int num_name, int _buttons_in_row) :
+    wxButton(parent, wxID_ANY, _names[(size_t)num_name], pos),
+    current_choice(num_name)
 {
-    Bind(wxEVT_BUTTON, &BmpButtonsCombo::OnButtonClicked, this);
+    Bind(wxEVT_BUTTON, &ButtonsCombo::OnButtonClicked, this);
 
-    SetToolTip(_tooltips[(size_t)num_file]);
+    SetToolTip(_tooltips[(size_t)num_name]);
 
     title = _title;
-    files = _files;
+    names = _names;
     tooltips = _tooltips;
     buttons_in_row = _buttons_in_row;
 }
 
 
-void BmpButtonsCombo::OnButtonClicked(wxCommandEvent &)
+void ButtonsCombo::OnButtonClicked(wxCommandEvent &)
 {
-    BmpButtonPopup *popup = new BmpButtonPopup(this, title, files, tooltips, buttons_in_row);
+    ButtonPopup *popup = new ButtonPopup(this, title, names, tooltips, buttons_in_row);
 
     wxPoint pos = ClientToScreen(wxPoint(GetSize().x / 2, GetSize().y / 2));
     pos.x -= popup->GetSize().x / 2;
@@ -100,15 +100,15 @@ void BmpButtonsCombo::OnButtonClicked(wxCommandEvent &)
 }
 
 
-void BmpButtonsCombo::SetCurrentChoice(int choice)
+void ButtonsCombo::SetCurrentChoice(int choice)
 {
     current_choice = choice;
 
-    SetFileBitmap(files[(uint)current_choice]);
+    SetLabel(names[(uint)current_choice]);
 }
 
 
-int BmpButtonsCombo::GetCurrentChoice() const
+int ButtonsCombo::GetCurrentChoice() const
 {
     return current_choice;
 }
---
+++ sources/VS/Linia2/src/Controls/ButtonsCombo.h
@@ -0,0 +1,32 @@
+﻿// 2025/8/9 11:41:23 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#include "Controls/Buttons.h"
+
+// Кнопка с рисунком, по нажатию на которую открывается окно с другими выборами
+
+
+class ButtonsCombo : public wxButton
+{
+public:
+
+    ButtonsCombo(wxWindow *parent, const wxString &title, const wxPoint &pos, const wxSize &,
+        const wxArrayString &names,             // Эти изображения будут на кнопках
+        const wxArrayString &tooltips,
+        int num_file,                           // Эта изображение будет на главной кнопке
+        int buttons_in_row);                    // В каждом ряду будет расположено столько кнопок
+
+    void SetCurrentChoice(int);
+
+    int GetCurrentChoice() const;
+
+private:
+
+    int current_choice = -1;
+
+    wxString title;
+    wxArrayString names;
+    wxArrayString tooltips;
+    int buttons_in_row;
+
+    void OnButtonClicked(wxCommandEvent &);
+};
