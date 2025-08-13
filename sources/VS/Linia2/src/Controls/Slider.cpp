71e3958d66a3b3ff4656c1720941bf15d7e72ae2
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-13 14:24:20 +0300

    *

---
+++ sources/VS/Linia2/src/Controls/Slider.cpp
@@ -0,0 +1,4 @@
+﻿// 2025/08/13 14:19:26 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Controls/Slider.h"
+
---
+++ sources/VS/Linia2/src/Controls/Slider.h
@@ -0,0 +1,9 @@
+﻿// 2025/08/13 14:18:41 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class Slider : public wxPanel
+{
+public:
+private:
+};
