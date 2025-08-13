3282913f404801965d53c02edbe17354b4206189
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-13 16:48:10 +0300

    refact

--- sources/VS/Linia2/src/Panels/Panel03_Config/Panel03_Config.cpp
+++ sources/VS/Linia2/src/Panels/Panel03_Config/Panel03_Config.cpp
@@ -93,10 +93,10 @@ void PanelConfig::CreatePanelChannelB(wxPanel *panel, int x, int w)
 {
     wxStaticBox *boxGenerator = new wxStaticBox(panel, wxID_ANY, _L("Генератор ступенек"), { x, 0 }, { w, 300 });
 
+    int width = 150;
+
     {
         int y = 25;
-
-        int width = 150;
 
         wxArrayString choices;
         choices.Add("U");
@@ -161,16 +161,14 @@ void PanelConfig::CreatePanelChannelB(wxPanel *panel, int x, int w)
     {
         int y = 20;
 
-        new wxStaticText(boxLimitation, wxID_ANY, _L("Диапазон"), { 10, SD::Y_SB(y) });
-
         wxArrayString choices;
         choices.Add("10 mA");
 
-        new wxComboBox(boxLimitation, wxID_ANY, choices[0], { 100, SD::Y_SB(y - 3) }, { 80, TEXTCNTRL_HEIGHT }, choices, wxCB_READONLY);
+        new ButtonsCombo(boxLimitation, "Диапазон", { 10, SD::Y_SB(y - 3) }, width, choices, 0, 1);
 
         y += 25;
 
-        new wxSlider(boxLimitation, wxID_ANY, 5, 0, 20, { 10, SD::Y_SB(y) }, { 120, TEXTCNTRL_HEIGHT });
+        new Slider(boxLimitation, { 10, SD::Y_SB(y) }, width);
     }
 }
 
