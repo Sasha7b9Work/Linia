0e8825f964739738c4737766d108aa8a8c6a00f5
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-20 16:11:37 +0300

    *

--- sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
@@ -72,11 +72,11 @@ public:
         }
 
         // Добавляем рамку вокруг сетки кнопок
-        wxStaticBoxSizer *boxSizer = new wxStaticBoxSizer(wxVERTICAL, mainPanel, GetCombo()->title);
-        boxSizer->Add(gridSizer, 1, wxEXPAND | wxALL, 5); // 10px отступ внутри рамки
+//        wxStaticBoxSizer *boxSizer = new wxStaticBoxSizer(wxVERTICAL, mainPanel, GetCombo()->title);
+//        boxSizer->Add(gridSizer, 1, wxEXPAND | wxALL, 5); // 10px отступ внутри рамки
 
         // Основная панель
-        mainPanel->SetSizer(boxSizer);
+        mainPanel->SetSizer(gridSizer);
 
         wxBoxSizer *outerSizer = new wxBoxSizer(wxVERTICAL);
         // Внешние отступы 15px
