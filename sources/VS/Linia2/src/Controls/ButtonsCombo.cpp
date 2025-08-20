43f4d606c7739d57f573bc2603abf8eebac6048a
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2025-08-20 20:35:01 +0300

    *

--- sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
@@ -35,7 +35,7 @@ public:
         wxArrayString &labels = GetCombo()->labels;
 
         // Основной контейнер с отступами по краям
-        wxPanel *mainPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+        wxPanel *mainPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_NONE);
 
         int num_rows = (int)(labels.size() / GetCombo()->buttons_in_row);
 
