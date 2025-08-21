d759dd2d501b49626eca71d64abe5c2e1a5ba336
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-21 11:27:34 +0300

    *

--- sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/ButtonsCombo.cpp
@@ -64,7 +64,7 @@ public:
             if (labels[i][0])                                                           // Признак того, что надо вставлять кнопку, а не заглушку
             {
                 wxButton *btn = new wxButton(mainPanel, wxID_ANY, labels[i]);
-                btn->SetBackgroundColour(btn->GetBackgroundColour().ChangeLightness((int)(LIGHTNESS * 1.5)));
+                btn->SetBackgroundColour(btn->GetBackgroundColour().ChangeLightness((int)(LIGHTNESS * 1.4)));
                 btn->SetMaxSize(wxSize(-1, 20));
                 if (labels[i] != GetCombo()->tooltips[i])
                 {
