ee2596ea0870999284b934fc3a796815ab2ecbf5
Sasha7b9Work <Phuntik-it@tut.by>
2025-08-21 11:30:15 +0300

    *

--- sources/VS/Linia2/src/Controls/BmpButtonsCombo.cpp
+++ sources/VS/Linia2/src/Controls/BmpButtonsCombo.cpp
@@ -38,6 +38,7 @@ public:
         for (uint i = 0; i < files.size(); ++i)
         {
             ButtonBitmap *btn = new ButtonBitmap(mainPanel, wxDefaultPosition, wxDefaultSize, files[i]);
+            btn->SetBackgroundColour(btn->GetBackgroundColour().ChangeLightness(120));
             btn->SetLabel(wxString::Format("%d", i));
             btn->SetToolTip(tooltips[i]);
             btn->Bind(wxEVT_BUTTON, &BmpButtonPopup::OnButtonClick, this);
