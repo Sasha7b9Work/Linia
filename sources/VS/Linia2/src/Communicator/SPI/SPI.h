864b8c93fdb0193da0d82c3d90134ebe671f2920
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-22 22:20:49 +0300

    форматирование

--- sources/VS/Linia2/src/Communicator/SPI/SPI.h
+++ sources/VS/Linia2/src/Communicator/SPI/SPI.h
@@ -5,25 +5,25 @@ namespace SPI
 {
     void Init();
     void DeInit();
-    
+
     // Основные функции для динамических DAC
     // \todo Переделать реализации. Функции не должны быть одинаковыми, а должны вызывать третью функцию с параметром.
     // Ещё лучше переделать к виду WriteDynamicDAC1(number_DAC, uint16_t value) - так уменьшается перегрузка интерфейса *.h-файла
     bool WriteDynamicDAC1(uint16_t value);
     bool WriteDynamicDAC2(uint16_t value);
-    
+
     // Настройки
     bool SetSpeed(uint32_t speedHz);
     bool SetMode(uint8_t mode);
-    
+
     // Состояние
     bool IsReady();
     uint32_t GetSpeed();
     uint8_t GetMode();
-    
+
     // Диагностические функции для Orange Pi
     void TestGPIO();
     void DiagnoseSPI();
-    bool CheckSPIDevice(const char* device_path = "/dev/spidev1.0");
+    bool CheckSPIDevice(const char *device_path = "/dev/spidev1.0");
     void PrintSystemInfo();
 }
