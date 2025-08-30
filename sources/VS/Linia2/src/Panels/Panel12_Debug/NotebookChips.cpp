1ba4c9293c747ee2c7c93161e502d71e7830c25b
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-30 12:10:24 +0300

    *

--- sources/VS/Linia2/src/Panels/Panel12_Debug/NotebookChips.cpp
+++ sources/VS/Linia2/src/Panels/Panel12_Debug/NotebookChips.cpp
@@ -2,8 +2,7 @@
 #include "defines.h"
 #include "Panels/Panel12_Debug/NotebookChips.h"
 #include "Panels/Panel12_Debug/Old/PageAD5697.h"
-#include "Panels/Panel12_Debug/Old/PageAD9952.h"
-#include "Panels/Panel12_Debug/Old/PageMCP4811.h"
+#include "Panels/Panel12_Debug/PageAD5300.h"
 
 
 NotebookChips *NotebookChips::self = nullptr;
@@ -14,13 +13,11 @@ NotebookChips::NotebookChips(wxWindow *parent) :
 {
     self = this;
 
+    new PageAD5300(this);
     new PageAD5697(this);
-//    new PageAD9952(this);
-//    new PageMCP4811(this);
 
+    wxNotebook::AddPage(PageAD5300::self, PageAD5300::self->GetName());
     wxNotebook::AddPage(PageAD5697::self, PageAD5697::self->GetName());
-//    wxNotebook::AddPage(PageAD9952::self, PageAD9952::self->GetName());
-//    wxNotebook::AddPage(PageMCP4811::self, PageMCP4811::self->GetName());
 
     wxWindowBase::Layout();
 }
--- sources/VS/Linia2/src/Panels/Panel12_Debug/PageAD5300.cpp
+++ sources/VS/Linia2/src/Panels/Panel12_Debug/PageAD5300.cpp
@@ -1,3 +1,42 @@
 ﻿// 2025/08/30 11:32:39 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/Panel12_Debug/PageAD5300.h"
+
+
+PageAD5300 *PageAD5300::self = nullptr;
+
+
+PageAD5300::PageAD5300(wxNotebook *parent) : PageChip(parent, "AD5300")
+{
+    self = this;
+
+    wxArrayString names;
+    for (int i = 0; i < 4; i++)
+    {
+        names.push_back("X");
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        names.push_back(wxString::Format("D%d", i));
+    }
+    names.push_back("PD0");
+    names.push_back("PD1");
+    for (int i = 0; i < 2; i++)
+    {
+        names.push_back("X");
+    }
+
+    PanelRegister *regInput = new PanelRegister(this, "Input Register", 16, false, false);
+
+    regInput->SetNamesBits(names);
+
+    std::vector<StructDescription> desc0;
+    desc0.push_back({ 0, 4, "not used" });
+    desc0.push_back({ 4, 8, "DATA BITS" });
+    desc0.push_back({ 12, 2, "POWER-DOWN MODES" });
+    desc0.push_back({ 14, 2, "not used" });
+
+    regInput->SetDescriptionBits(0, desc0);
+
+    AppendRegister(regInput);
+}
