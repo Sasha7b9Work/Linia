2a1ee81faec1c4ae7a3224603726e679f4a6c508
Sasha7b9Work <Sasha7b9@tut.by>
2025-08-31 20:15:51 +0300

    *

---
+++ sources/VS/Linia2/src/Storage/Record.h
@@ -0,0 +1,10 @@
+﻿// 2025/08/31 20:13:54 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+// Здесь хранится одно измерение - например, семейство ВАХ биполярного транзистора
+struct Record
+{
+
+};
+
