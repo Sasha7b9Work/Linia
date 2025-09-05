ed09b19e18405118605f5fe0557cdd75d77398af
Sasha7b9Work <Phuntik-it@tut.by>
2025-09-05 11:00:15 +0300

    *

--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 67
-#define DATE_BUILD "2025-09-05 10:33:03"
+#define VERSION_BUILD 68
+#define DATE_BUILD "2025-09-05 11:00:15"
 
 
 #define WIN32_LEAN_AND_MEAN
