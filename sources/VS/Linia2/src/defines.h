6e816fe12884a2c5bfffaba59cbc9a9246475a80
Sasha7b9Work <Sasha7b9@tut.by>
2025-09-05 20:30:45 +0300

    Начинаем компиляцию gpio.cpp под Windows

--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 76
-#define DATE_BUILD "2025-09-05 20:24:03"
+#define VERSION_BUILD 77
+#define DATE_BUILD "2025-09-05 20:30:45"
 
 
 #define WIN32_LEAN_AND_MEAN
