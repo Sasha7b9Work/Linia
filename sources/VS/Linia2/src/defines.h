f67093b8791a0d950cc9c9ebd549d6ebffface86
Sasha7b9Work <Phuntik-it@tut.by>
2025-09-05 11:24:53 +0300

    Добавлены ресурсы

--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 72
-#define DATE_BUILD "2025-09-05 11:24:41"
+#define VERSION_BUILD 73
+#define DATE_BUILD "2025-09-05 11:24:52"
 
 
 #define WIN32_LEAN_AND_MEAN
