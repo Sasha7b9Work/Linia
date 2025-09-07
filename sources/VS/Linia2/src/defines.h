90006be1fd71bcb04fd4afe2053356134d072e1d
Sasha7b9Work <Sasha7b9@tut.by>
2025-09-07 22:35:09 +0300

    *

--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 149
-#define DATE_BUILD "2025-09-07 22:29:26"
+#define VERSION_BUILD 150
+#define DATE_BUILD "2025-09-07 22:35:09"
 
 
 #define WIN32_LEAN_AND_MEAN
