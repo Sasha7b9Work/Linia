9394aa3d16dad55fa86816dcb8c7b8695ad1dd12
Sasha7b9Work <Phuntik-it@tut.by>
2025-09-08 11:09:48 +0300

    detrash

--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 153
-#define DATE_BUILD "2025-09-08 08:45:42"
+#define VERSION_BUILD 154
+#define DATE_BUILD "2025-09-08 11:09:48"
 
 
 #define WIN32_LEAN_AND_MEAN
