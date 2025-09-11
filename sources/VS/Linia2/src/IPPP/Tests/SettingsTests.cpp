5fa1b72b0cb2b18b8bc58d7f17a0e7868335d2dd
Sasha7b9Work <Phuntik-it@tut.by>
2025-09-11 09:05:28 +0300

    *

--- sources/VS/Linia2/src/IPPP/Tests/SettingsTests.cpp
+++ sources/VS/Linia2/src/IPPP/Tests/SettingsTests.cpp
@@ -21,28 +21,30 @@ bool Category::IsGDS()
 
 wxString Chan::Name(E chan)
 {
-    switch (chan) {
-        case _C: return "C";
-        case _B: return "B";
-        case _S: return "S";
-        case _E: return "E";
-        case Count:
-        default: return "";
+    switch (chan)
+    {
+    case _C: return "C";
+    case _B: return "B";
+    case _S: return "S";
+    case _E: return "E";
+    case Count:
+    default: return "";
     }
 }
 
 wxString StateJack::Name(E state)
 {
-    switch (state) {
-        case _C: return "C";
-        case _B: return "B";
-        case _S: return "S";
-        case _E: return "E";
-        case General: return "GENERAL";
-        case General_1k: return "GENERAL_1K";
-        case Break: return "BREAK";
-        case Count:
-        default: return "BREAK";
+    switch (state)
+    {
+    case _C: return "C";
+    case _B: return "B";
+    case _S: return "S";
+    case _E: return "E";
+    case General: return "GENERAL";
+    case General_1k: return "GENERAL_1K";
+    case Break: return "BREAK";
+    case Count:
+    default: return "BREAK";
     }
 }
 
@@ -58,16 +60,17 @@ wxString ModeMeas::Name(E mode)
 
 wxString TypeScan::Name(E type)
 {
-    switch (type) {
-        case ImpulsePos: return "IMP_P";
-        case ImpulseNeg: return "IMP_N";
-        case DCPos: return "DC_P";
-        case DCNeg: return "DC_N";
-        case SYNPos: return "SYN_P";
-        case SYNNeg: return "SYN_N";
-        case AC: return "AC";
-        case Count:
-        default: return "";
+    switch (type)
+    {
+    case ImpulsePos: return "IMP_P";
+    case ImpulseNeg: return "IMP_N";
+    case DCPos: return "DC_P";
+    case DCNeg: return "DC_N";
+    case SYNPos: return "SYN_P";
+    case SYNNeg: return "SYN_N";
+    case AC: return "AC";
+    case Count:
+    default: return "";
     }
 }
 
@@ -101,44 +104,47 @@ wxString StartMode::Name(E mode)
 
 wxString MeasMode::Name(E mode)
 {
-    switch (mode) {
-        case Manual: return "MANUAL";
-        case Auto_1: return "AUTO_1";
-        case Auto_2: return "AUTO_2";
-        case Auto_3: return "AUTO_3";
-        case Auto_4: return "AUTO_4";
-        case Auto_5: return "AUTO_5";
-        case Count:
-        default: return "MANUAL";
+    switch (mode)
+    {
+    case Manual: return "MANUAL";
+    case Auto_1: return "AUTO_1";
+    case Auto_2: return "AUTO_2";
+    case Auto_3: return "AUTO_3";
+    case Auto_4: return "AUTO_4";
+    case Auto_5: return "AUTO_5";
+    case Count:
+    default: return "MANUAL";
     }
 }
 
 
 wxString DacCode::Name(E code)
 {
-    switch (code) {
-        case Code_100: return "100";
-        case Code_110: return "110";
-        case Code_120: return "120";
-        case Code_130: return "130";
-        case Code_140: return "140";
-        case Code_150: return "150";
-        case Count:
-        default: return "130";
+    switch (code)
+    {
+    case Code_100: return "100";
+    case Code_110: return "110";
+    case Code_120: return "120";
+    case Code_130: return "130";
+    case Code_140: return "140";
+    case Code_150: return "150";
+    case Count:
+    default: return "130";
     }
 }
 
 uint8_t DacCode::Value(E code)
 {
-    switch (code) {
-        case Code_100: return 100;
-        case Code_110: return 110;
-        case Code_120: return 120;
-        case Code_130: return 130;
-        case Code_140: return 140;
-        case Code_150: return 150;
-        case Count:
-        default: return 130;
+    switch (code)
+    {
+    case Code_100: return 100;
+    case Code_110: return 110;
+    case Code_120: return 120;
+    case Code_130: return 130;
+    case Code_140: return 140;
+    case Code_150: return 150;
+    case Count:
+    default: return 130;
     }
 }
 
@@ -157,13 +163,14 @@ wxString ElementType::Name(E type)
 
 wxString LimitResult::Name(E result)
 {
-    switch (result) {
-        case Pass: return "PASS";
-        case Fail: return "FAIL";
-        case NotReached: return "NOT_REACHED";
-        case Protection: return "PROTECTION";
-        case Count:
-        default: return "PASS";
+    switch (result)
+    {
+    case Pass: return "PASS";
+    case Fail: return "FAIL";
+    case NotReached: return "NOT_REACHED";
+    case Protection: return "PROTECTION";
+    case Count:
+    default: return "PASS";
     }
 }
 
@@ -176,61 +183,64 @@ wxString PowerNominal::Name(E nominal)
 
 wxString PulseDuration::Name(E duration)
 {
-    switch (duration) {
-        case Duration_50us: return "50us";
-        case Duration_100us: return "100us";
-        case Duration_200us: return "200us";
-        case Duration_300us: return "300us";
-        case Duration_400us: return "400us";
-        case Duration_500us: return "500us";
-        case Duration_1000us: return "1000us";
-        case Count:
-        default: return "200us";
+    switch (duration)
+    {
+    case Duration_50us: return "50us";
+    case Duration_100us: return "100us";
+    case Duration_200us: return "200us";
+    case Duration_300us: return "300us";
+    case Duration_400us: return "400us";
+    case Duration_500us: return "500us";
+    case Duration_1000us: return "1000us";
+    case Count:
+    default: return "200us";
     }
 }
 
 uint PulseDuration::Value(E duration)
 {
-    switch (duration) {
-        case Duration_50us: return 50;
-        case Duration_100us: return 100;
-        case Duration_200us: return 200;
-        case Duration_300us: return 300;
-        case Duration_400us: return 400;
-        case Duration_500us: return 500;
-        case Duration_1000us: return 1000;
-        case Count:
-        default: return 200;
+    switch (duration)
+    {
+    case Duration_50us: return 50;
+    case Duration_100us: return 100;
+    case Duration_200us: return 200;
+    case Duration_300us: return 300;
+    case Duration_400us: return 400;
+    case Duration_500us: return 500;
+    case Duration_1000us: return 1000;
+    case Count:
+    default: return 200;
     }
 }
 
 
 wxString LimitPercent::Name(E percent)
 {
-    switch (percent) {
-        case Percent_0: return "0%";
-        case Percent_5: return "5%";
-        case Percent_10: return "10%";
-        case Percent_15: return "15%";
-        case Percent_20: return "20%";
-        case Percent_25: return "25%";
-        case Percent_30: return "30%";
-        case Percent_35: return "35%";
-        case Percent_40: return "40%";
-        case Percent_45: return "45%";
-        case Percent_50: return "50%";
-        case Percent_55: return "55%";
-        case Percent_60: return "60%";
-        case Percent_65: return "65%";
-        case Percent_70: return "70%";
-        case Percent_75: return "75%";
-        case Percent_80: return "80%";
-        case Percent_85: return "85%";
-        case Percent_90: return "90%";
-        case Percent_95: return "95%";
-        case Percent_100: return "100%";
-        case Count:
-        default: return "0%";
+    switch (percent)
+    {
+    case Percent_0: return "0%";
+    case Percent_5: return "5%";
+    case Percent_10: return "10%";
+    case Percent_15: return "15%";
+    case Percent_20: return "20%";
+    case Percent_25: return "25%";
+    case Percent_30: return "30%";
+    case Percent_35: return "35%";
+    case Percent_40: return "40%";
+    case Percent_45: return "45%";
+    case Percent_50: return "50%";
+    case Percent_55: return "55%";
+    case Percent_60: return "60%";
+    case Percent_65: return "65%";
+    case Percent_70: return "70%";
+    case Percent_75: return "75%";
+    case Percent_80: return "80%";
+    case Percent_85: return "85%";
+    case Percent_90: return "90%";
+    case Percent_95: return "95%";
+    case Percent_100: return "100%";
+    case Count:
+    default: return "0%";
     }
 }
 
@@ -242,13 +252,14 @@ int LimitPercent::Value(E percent)
 
 wxString AmplitudeRange::Name(E range)
 {
-    switch (range) {
-        case Range_200mV: return "200mV";
-        case Range_2V: return "2V";
-        case Range_20V: return "20V";
-        case Range_200V: return "200V";
-        case Count:
-        default: return "200mV";
+    switch (range)
+    {
+    case Range_200mV: return "200mV";
+    case Range_2V: return "2V";
+    case Range_20V: return "20V";
+    case Range_200V: return "200V";
+    case Count:
+    default: return "200mV";
     }
 }
 
@@ -260,96 +271,100 @@ uint8_t AmplitudeRange::Value(E range)
 
 wxString StepCount::Name(E count)
 {
-    switch (count) {
-        case Steps_1: return "1";
-        case Steps_10: return "10";
-        case Steps_20: return "20";
-        case Steps_50: return "50";
-        case Steps_100: return "100";
-        case Steps_200: return "200";
-        case Steps_500: return "500";
-        case Steps_1000: return "1000";
-        case Count:
-        default: return "1";
+    switch (count)
+    {
+    case Steps_1: return "1";
+    case Steps_10: return "10";
+    case Steps_20: return "20";
+    case Steps_50: return "50";
+    case Steps_100: return "100";
+    case Steps_200: return "200";
+    case Steps_500: return "500";
+    case Steps_1000: return "1000";
+    case Count:
+    default: return "1";
     }
 }
 
 int StepCount::Value(E count)
 {
-    switch (count) {
-        case Steps_1: return 1;
-        case Steps_10: return 10;
-        case Steps_20: return 20;
-        case Steps_50: return 50;
-        case Steps_100: return 100;
-        case Steps_200: return 200;
-        case Steps_500: return 500;
-        case Steps_1000: return 1000;
-        case Count:
-        default: return 1;
+    switch (count)
+    {
+    case Steps_1: return 1;
+    case Steps_10: return 10;
+    case Steps_20: return 20;
+    case Steps_50: return 50;
+    case Steps_100: return 100;
+    case Steps_200: return 200;
+    case Steps_500: return 500;
+    case Steps_1000: return 1000;
+    case Count:
+    default: return 1;
     }
 }
 
 
 wxString AmplitudeValue::Name(E value)
 {
-    switch (value) {
-        case Value_N32767: return "-32767";
-        case Value_N30000: return "-30000";
-        case Value_N20000: return "-20000";
-        case Value_N10000: return "-10000";
-        case Value_N5000: return "-5000";
-        case Value_N1000: return "-1000";
-        case Value_N500: return "-500";
-        case Value_N100: return "-100";
-        case Value_N50: return "-50";
-        case Value_N10: return "-10";
-        case Value_N1: return "-1";
-        case Value_0: return "0";
-        case Value_1: return "1";
-        case Value_10: return "10";
-        case Value_50: return "50";
-        case Value_100: return "100";
-        case Value_500: return "500";
-        case Value_1000: return "1000";
-        case Value_5000: return "5000";
-        case Value_10000: return "10000";
-        case Value_20000: return "20000";
-        case Value_30000: return "30000";
-        case Value_32767: return "32767";
-        case Count:
-        default: return "0";
+    switch (value)
+    {
+    case Value_N32767: return "-32767";
+    case Value_N30000: return "-30000";
+    case Value_N20000: return "-20000";
+    case Value_N10000: return "-10000";
+    case Value_N5000: return "-5000";
+    case Value_N1000: return "-1000";
+    case Value_N500: return "-500";
+    case Value_N100: return "-100";
+    case Value_N50: return "-50";
+    case Value_N10: return "-10";
+    case Value_N1: return "-1";
+    case Value_0: return "0";
+    case Value_1: return "1";
+    case Value_10: return "10";
+    case Value_50: return "50";
+    case Value_100: return "100";
+    case Value_500: return "500";
+    case Value_1000: return "1000";
+    case Value_5000: return "5000";
+    case Value_10000: return "10000";
+    case Value_20000: return "20000";
+    case Value_30000: return "30000";
+    case Value_32767: return "32767";
+    case Count:
+    default: return "0";
     }
 }
 
 int16_t AmplitudeValue::Value(E value)
 {
-    switch (value) {
-        case Value_N32767: return -32767;
-        case Value_N30000: return -30000;
-        case Value_N20000: return -20000;
-        case Value_N10000: return -10000;
-        case Value_N5000: return -5000;
-        case Value_N1000: return -1000;
-        case Value_N500: return -500;
-        case Value_N100: return -100;
-        case Value_N50: return -50;
-        case Value_N10: return -10;
-        case Value_N1: return -1;
-        case Value_0: return 0;
-        case Value_1: return 1;
-        case Value_10: return 10;
-        case Value_50: return 50;
-        case Value_100: return 100;
-        case Value_500: return 500;
-        case Value_1000: return 1000;
-        case Value_5000: return 5000;
-        case Value_10000: return 10000;
-        case Value_20000: return 20000;
-        case Value_30000: return 30000;
-        case Value_32767: return 32767;
-        case Count:
-        default: return 0;
+    switch (value)
+    {
+    case Value_N32767: return -32767;
+    case Value_N30000: return -30000;
+    case Value_N20000: return -20000;
+    case Value_N10000: return -10000;
+    case Value_N5000: return -5000;
+    case Value_N1000: return -1000;
+    case Value_N500: return -500;
+    case Value_N100: return -100;
+    case Value_N50: return -50;
+    case Value_N10: return -10;
+    case Value_N1: return -1;
+    case Value_0: return 0;
+    case Value_1: return 1;
+    case Value_10: return 10;
+    case Value_50: return 50;
+    case Value_100: return 100;
+    case Value_500: return 500;
+    case Value_1000: return 1000;
+    case Value_5000: return 5000;
+    case Value_10000: return 10000;
+    case Value_20000: return 20000;
+    case Value_30000: return 30000;
+    case Value_32767: return 32767;
+    case Count:
+    default: return 0;
     }
 }
 
--- sources/VS/Linia2/src/defines.h
+++ sources/VS/Linia2/src/defines.h
@@ -2,8 +2,8 @@
 #pragma once
 
 
-#define VERSION_BUILD 204
-#define DATE_BUILD "2025-09-11 09:04:17"
+#define VERSION_BUILD 205
+#define DATE_BUILD "2025-09-11 09:05:27"
 
 
 #define WIN32_LEAN_AND_MEAN
