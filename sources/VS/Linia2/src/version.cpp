27d4af9b302ccb9e59abadef511091aac224936f
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-08 08:01:07 +0300

    Кажется, работает связь по UART

--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 487 ;
-const char *DATE_BUILD = "2025-10-08 07:59:53" ;
+unsigned int VERSION_BUILD = 488 ;
+const char *DATE_BUILD = "2025-10-08 08:01:07" ;
