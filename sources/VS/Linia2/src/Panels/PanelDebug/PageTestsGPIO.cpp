9a1a3bc895cf7b0f6b40336ed98cdeebbd148f8b
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-08 11:35:57 +0300

    *

--- sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
+++ sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
@@ -417,7 +417,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
     {
         gpiod_line *infoF0 = GPIO::GetInputPinInfo(Pin::In_DAT_F0)->hw.line;
         gpiod_line *infoCS = GPIO::GetOutputPinInfo(Pin::Out_SPI_CS)->hw.line;
-//        gpiod_line *infoREQ = GPIO::GetOutputPinInfo(Pin::Out_REQ_RD)->hw.line;
+        gpiod_line *infoREQ = GPIO::GetOutputPinInfo(Pin::Out_REQ_RD)->hw.line;
 
         TimeMeterMS meter;
 
@@ -437,7 +437,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
 
                 for (int i = 7; i >= 0; i--)
                 {
-//                    PinOut::Set(infoREQ, 1);
+                    PinOut::Set(infoREQ, 1);
 
                     if (PinIn::GetHardware(infoF0))
                     {
@@ -448,7 +448,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
                         byte |= 0;
                     }
 
-//                    PinOut::Set(infoREQ, 0);
+                    PinOut::Set(infoREQ, 0);
                 }
 
                 bytes[b] = byte;
--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 528 ;
-const char *DATE_BUILD = "2025-10-08 11:35:02" ;
+unsigned int VERSION_BUILD = 529 ;
+const char *DATE_BUILD = "2025-10-08 11:35:57" ;
