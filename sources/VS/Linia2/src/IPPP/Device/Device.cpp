b0b0b75ced22e48bb13b804e2e5059f9e4f16f3d
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-08 08:03:49 +0300

    *

--- sources/VS/Linia2/src/IPPP/Device/Device.cpp
+++ sources/VS/Linia2/src/IPPP/Device/Device.cpp
@@ -39,7 +39,7 @@ void Device::Shutdown()
 }
 
 
-void Device::CallbackOnReceive(uint8 byte)
+void Device::CallbackOnReceive(uint8 /*byte*/)
 {
 //    LOG_WRITE("receive %c", (char)byte);
 }
--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 488 ;
-const char *DATE_BUILD = "2025-10-08 08:01:07" ;
+unsigned int VERSION_BUILD = 489 ;
+const char *DATE_BUILD = "2025-10-08 08:03:48" ;
