62a25fba72c3d612747237e80f2a3c5134de7961
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-08 08:07:17 +0300

    Скорость UART увеличена до 2000000

--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 489 ;
-const char *DATE_BUILD = "2025-10-08 08:03:48" ;
+unsigned int VERSION_BUILD = 490 ;
+const char *DATE_BUILD = "2025-10-08 08:07:17" ;
