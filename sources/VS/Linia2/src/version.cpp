ba4cda7935f1d3b446a7a70cde41b7a95751f88d
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-09 08:07:11 +0300

    Подключен опрос портов ввода-вывода

--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 529 ;
-const char *DATE_BUILD = "2025-10-08 11:35:57" ;
+unsigned int VERSION_BUILD = 530 ;
+const char *DATE_BUILD = "2025-10-09 08:07:11" ;
