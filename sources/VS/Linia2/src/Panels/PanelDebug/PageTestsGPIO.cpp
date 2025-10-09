17fd029a5bca5e97c47e08121c5d7b1021aef02b
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-09 08:19:47 +0300

    *

--- sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
+++ sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
@@ -415,7 +415,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
 
     if (pinFIFO_FULL.Get() && prev == false)
     {
-        gpiod_line *infoF0 = GPIO::GetInputPinInfo(Pin::In_DAT_F0)->hw.line;
+//        gpiod_line *infoF0 = GPIO::GetInputPinInfo(Pin::In_DAT_F0)->hw.line;
         gpiod_line *infoCS = GPIO::GetOutputPinInfo(Pin::Out_SPI_CS)->hw.line;
         gpiod_line *infoREQ = GPIO::GetOutputPinInfo(Pin::Out_REQ_RD)->hw.line;
 
