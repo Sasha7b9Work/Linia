3d3dda428830d8949cad50296c67a57ec40a9635
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-09 09:11:23 +0300

    *

--- sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
+++ sources/VS/Linia2/src/Panels/PanelDebug/PageTestsGPIO.cpp
@@ -365,7 +365,7 @@ void PageTestsGPIO::Update()
     {
         LOG_WRITE("time FPGA last = %.1f ms, ave = %.1f ms", time_FPGA, time_ave_FPGA);
         LOG_WRITE("time pin get = %.1f us, time pin set = %.1f us", PinIn::TimeGetAverage(), PinOut::TimeSetAverage());
-        LOG_WRITE("time pin get raw = %.1f ns, time pin set raw = %.1f ns", time_get_pin, time_set_pin);
+        LOG_WRITE("time pin get raw = %.1f us, time pin set raw = %.1f us", time_get_pin, time_set_pin);
         time_FPGA = 0.0f;
     }
 }
@@ -430,7 +430,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
         }
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        PageTestsGPIO::self->time_set_pin = (float)duration.count();
+        PageTestsGPIO::self->time_set_pin = (float)duration.count() / (float)num_meas;
 
         start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < num_meas; i++)
@@ -439,7 +439,7 @@ void PageTestsGPIO::ThreadFuncFPGA()
         }
         end = std::chrono::high_resolution_clock::now();
         duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        PageTestsGPIO::self->time_get_pin = (float)duration.count();
+        PageTestsGPIO::self->time_get_pin = (float)duration.count() / (float)num_meas;
 
         TimeMeterMS meter;
 
