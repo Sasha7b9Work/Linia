6254ed139f6187320ca1dd67f7d475ced705f24a
Sasha7b9Work <Sasha7b9@tut.by>
2025-10-15 22:08:18 +0300

    *

--- sources/VS/Linia2/src/Panels/PanelDebug/PagesBlocks/PageSource50V.cpp
+++ sources/VS/Linia2/src/Panels/PanelDebug/PagesBlocks/PageSource50V.cpp
@@ -16,8 +16,6 @@ PageSource50V::PageSource50V(wxNotebook *parent) :
 {
     self = this;
 
-    AppendRegister(new RegFPGA(this, "REG6", 8));
-
     RegAD5543 *regDAC7 = new RegAD5543(this, "DAC6");
 
     regDAC7->Disable();
