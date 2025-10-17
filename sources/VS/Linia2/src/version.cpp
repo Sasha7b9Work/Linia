16225d059f57b3f46e584d4cf8ccf7cb8c654e2f
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-17 09:28:52 +0300

    Подключена автоматическая перезагрузка при необнаружении UART либо SPI

--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 633 ;
-const char *DATE_BUILD = "2025-10-17 09:27:12" ;
+unsigned int VERSION_BUILD = 634 ;
+const char *DATE_BUILD = "2025-10-17 09:28:52" ;
