1c8091098bdcca49c3ccb68aa59c630b89bf04ae
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-17 15:34:56 +0300

    *

--- sources/VS/Linia2/src/Application.cpp
+++ sources/VS/Linia2/src/Application.cpp
@@ -113,13 +113,13 @@ bool Application::OnInit()
     PanelErrors::self->AppendError(Error::_1, "Мало памяти");
     PanelErrors::self->AppendError(Error::_2, "Много памяти");
 
-    for (int i = 0; i < 10; i++)
-    {
-        PanelErrors::self->AppendError(Error::_1, wxString::Format("Ошибка %d", i));
-    }
+//    for (int i = 0; i < 10; i++)
+//    {
+//        PanelErrors::self->AppendError(Error::_1, wxString::Format("Ошибка %d", i));
+//    }
 
-//    PanelErrors::self->RemoveError(Error::_1, "Мало памяти");
-//    PanelErrors::self->RemoveError(Error::_2, "Много памяти");
+    PanelErrors::self->RemoveError(Error::_1, "Мало памяти");
+    PanelErrors::self->RemoveError(Error::_2, "Много памяти");
 
     if (!SoftTests::RunAll())
     {
