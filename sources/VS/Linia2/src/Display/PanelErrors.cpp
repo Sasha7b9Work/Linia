f6d595a2f14d9d79241fa8b90944f4c16802729a
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-17 13:57:07 +0300

    *

--- sources/VS/Linia2/src/Display/PanelErrors.cpp
+++ sources/VS/Linia2/src/Display/PanelErrors.cpp
@@ -15,8 +15,8 @@ PanelErrors::PanelErrors(wxWindow *parent) :
     self = this;
 
     text_ctrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, { 0, 0 }, { MainWindow::WIDTH2, 90 }, wxTE_MULTILINE | wxTE_READONLY | wxNO_BORDER | wxTE_NO_VSCROLL);
-    text_ctrl->SetForegroundColour(*wxWHITE);
-    text_ctrl->SetBackgroundColour(*wxRED);
+//    text_ctrl->SetForegroundColour(*wxWHITE);
+//    text_ctrl->SetBackgroundColour(*wxRED);
 
     wxSize size_button{ 90, 22 };
     btnCollapse = new wxButton(this, wxID_ANY, "Свернуть", { Display::self->GetSize().x - size_button.x - 10, SD::Y_SB(0) }, size_button);
--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 669 ;
-const char *DATE_BUILD = "2025-10-17 13:56:17" ;
+unsigned int VERSION_BUILD = 670 ;
+const char *DATE_BUILD = "2025-10-17 13:57:06" ;
