be7aa981c332655b8732f35d5eb0967cb0a393ab
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-22 08:45:05 +0300

    *

--- sources/VS/Linia2/src/Application.cpp
+++ sources/VS/Linia2/src/Application.cpp
@@ -93,7 +93,7 @@ bool Application::OnInit()
 
         AutoRebootDialog dialog(frame, message, 10, []
             {
-                (void)std::system("reboot -f");
+                (void)std::system("shutdown -r now");
             });
 
         dialog.ShowModal();
@@ -107,7 +107,7 @@ bool Application::OnInit()
 
         AutoRebootDialog dialog(frame, message, 10, []
             {
-                (void)std::system("reboot -f");
+                (void)std::system("shutdown -r now");
             });
 
         dialog.ShowModal();
--- sources/VS/Linia2/src/version.cpp
+++ sources/VS/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 723 ;
-const char *DATE_BUILD = "2025-10-22 08:24:25" ;
+unsigned int VERSION_BUILD = 724 ;
+const char *DATE_BUILD = "2025-10-22 08:45:04" ;
