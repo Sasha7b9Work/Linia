2556cf95d861e3f5686801e913a41723e4240810
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-31 14:12:57 +0300

    refact

--- sources/scripts/cb_lin/full_build.sh
+++ sources/scripts/cb_lin/full_build.sh
@@ -2,6 +2,6 @@
 
 rm -R -f ../../generated
 
-cmake ../../VS/CMakeLists.txt -B../../generated -G "CodeBlocks - Unix Makefiles"
+cmake ../../CMakeLists.txt -B../../generated -G "CodeBlocks - Unix Makefiles"
 
 ./build.sh
