d18877e6a57ce4df3ab0a4117026d84a4e9dae90
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-14 09:01:29 +0300

    *

--- sources/ThirdParty/linux/make.sh
+++ sources/ThirdParty/linux/make.sh
@@ -3,4 +3,4 @@
 cd ../wxWidgets
 rm -R -f generated
 cd ../linux
-cmake ../wxWidgets/CMakeLists.txt -B../wxWidgets/generated -G "CodeBlocks - Unix Makefiles" -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=OFF -DwxBUILD_STRIPPED_RELEASE=ON -DCMAKE_CXX_FLAGS="-pipe" -DCMAKE_C_FLAGS="-pipe"
+cmake ../wxWidgets/CMakeLists.txt -B../wxWidgets/generated -G "CodeBlocks - Unix Makefiles" -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=OFF -DwxBUILD_STRIPPED_RELEASE=ON -DCMAKE_CXX_FLAGS_INIT="-pipe" -DCMAKE_C_FLAGS_INIT="-pipe"
