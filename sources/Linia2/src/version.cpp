00034d4808abfcc8fb50b5d659672a0cd2715f74
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-14 12:18:47 +0300

    Восстановлена компилируемость на windows

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 852 ;
-const char *DATE_BUILD = "2025-11-10 09:44:44" ;
+unsigned int VERSION_BUILD = 853 ;
+const char *DATE_BUILD = "2025-11-14 12:18:47" ;
