f79bd446b76f588393c6b83e158bb2c2918de82d
Sasha7b9 <sasha7b9@tut.by>
2025-11-21 09:57:23 +0300

    *

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 858 ;
-const char *DATE_BUILD = "2025-11-21 09:56:35" ;
+unsigned int VERSION_BUILD = 859 ;
+const char *DATE_BUILD = "2025-11-21 09:57:23" ;
