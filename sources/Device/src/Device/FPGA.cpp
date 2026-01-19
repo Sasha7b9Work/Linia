614ba9325aa40a611cbf54d6722d0ac12536806c
Sasha7b9Work <Phuntik-it@tut.by>
2026-01-19 12:10:33 +0300

    Добавили файлы USBD. Не компилируется

--- sources/Device/src/Device/FPGA.cpp
+++ sources/Device/src/Device/FPGA.cpp
@@ -48,17 +48,15 @@ void FPGA::Init()
 
 void FPGA::Update()
 {
-/*
-    Reg::Write(0, 1);
+    Reg::Write(0, 2);
 
     Timer::DelayMS(1000);
 
     WriteStart();
 
     Timer::DelayMS(2000);
-*/
 
-//    /*
+    /*
     if (!is_running_scan)
     {
         return;
@@ -69,7 +67,7 @@ void FPGA::Update()
         WriteStart();
         meter.Reset();
     }
-//    */
+    */
 }
 
 
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 916 ;
-const char *DATE_BUILD = "2026-01-16 08:44:40" ;
+unsigned int VERSION_BUILD = 917 ;
+const char *DATE_BUILD = "2026-01-19 12:10:32" ;
