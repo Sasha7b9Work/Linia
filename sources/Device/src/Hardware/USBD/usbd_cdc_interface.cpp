07c0dfd541a4008dee6ccf4a4df713c79dda2df7
Sasha7b9Work <Phuntik-it@tut.by>
2026-01-20 10:32:07 +0300

    Device refact

--- sources/Device/src/Hardware/USBD/usbd_cdc_interface.cpp
+++ sources/Device/src/Hardware/USBD/usbd_cdc_interface.cpp
@@ -15,7 +15,7 @@
   *
   ******************************************************************************
   */
-/* Includes ------------------------------------------------------------------ */
+  /* Includes ------------------------------------------------------------------ */
 #include "defines.h"
 #include "usbd_cdc.h"
 #include "Hardware/USBD/usbd_cdc_interface.h"
@@ -46,10 +46,10 @@ USBD_CDC_LineCodingTypeDef LineCoding = {
   0x08                          /* nb. of bits 8 */
 };
 
-uint8_t UserRxBuffer[APP_RX_DATA_SIZE]; /* Received Data over USB are stored in 
+uint8_t UserRxBuffer[APP_RX_DATA_SIZE]; /* Received Data over USB are stored in
                                          * this buffer */
 uint8_t UserTxBuffer[APP_TX_DATA_SIZE]; /* Received Data over UART (CDC
-                                         * interface) are stored in this buffer 
+                                         * interface) are stored in this buffer
                                          */
 
 uint32_t UserTxBufPtrIn = 0;    /* Increment this pointer or roll it back to
@@ -58,14 +58,14 @@ uint32_t UserTxBufPtrIn = 0;    /* Increment this pointer or roll it back to
 uint32_t UserTxBufPtrOut = 0;   /* Increment this pointer or roll it back to
                                  * start address when data are sent over USB */
 
-/* USB handler declaration */
+                                 /* USB handler declaration */
 extern USBD_HandleTypeDef USBD_Device;
 
 /* Private function prototypes ----------------------------------------------- */
 static int8_t CDC_Itf_Init(void);
 static int8_t CDC_Itf_DeInit(void);
-static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t * pbuf, uint16_t length);
-static int8_t CDC_Itf_Receive(uint8_t* pbuf, uint32_t *Len);
+static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t *pbuf, uint16_t length);
+static int8_t CDC_Itf_Receive(uint8_t *pbuf, uint32_t *Len);
 static int8_t CDC_Itf_TransmitCplt(uint8_t *pbuf, uint32_t *Len, uint8_t epnum);
 
 
@@ -80,17 +80,17 @@ USBD_CDC_ItfTypeDef USBD_CDC_fops = {
 /* Private functions --------------------------------------------------------- */
 
 /**
-  * @brief  Initializes the CDC media low layer      
+  * @brief  Initializes the CDC media low layer
   * @param  None
   * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
   */
 static int8_t CDC_Itf_Init(void)
 {
-  /* ##-5- Set Application Buffers ############################################ */
-  USBD_CDC_SetTxBuffer(&USBD_Device, UserTxBuffer, 0);
-  USBD_CDC_SetRxBuffer(&USBD_Device, UserRxBuffer);
+    /* ##-5- Set Application Buffers ############################################ */
+    USBD_CDC_SetTxBuffer(&USBD_Device, UserTxBuffer, 0);
+    USBD_CDC_SetRxBuffer(&USBD_Device, UserRxBuffer);
 
-  return (USBD_OK);
+    return (USBD_OK);
 }
 
 /**
@@ -101,78 +101,78 @@ static int8_t CDC_Itf_Init(void)
   */
 static int8_t CDC_Itf_DeInit(void)
 {
-  return (USBD_OK);
+    return (USBD_OK);
 }
 
 /**
   * @brief  CDC_Itf_Control
   *         Manage the CDC class requests
-  * @param  Cmd: Command code            
+  * @param  Cmd: Command code
   * @param  Buf: Buffer containing command data (request parameters)
   * @param  Len: Number of data to be sent (in bytes)
   * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
   */
-static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t * pbuf, uint16_t /*length*/)
+static int8_t CDC_Itf_Control(uint8_t cmd, uint8_t *pbuf, uint16_t /*length*/)
 {
-  switch (cmd)
-  {
-  case CDC_SEND_ENCAPSULATED_COMMAND:
-    /* Add your code here */
-    break;
+    switch (cmd)
+    {
+    case CDC_SEND_ENCAPSULATED_COMMAND:
+        /* Add your code here */
+        break;
 
-  case CDC_GET_ENCAPSULATED_RESPONSE:
-    /* Add your code here */
-    break;
+    case CDC_GET_ENCAPSULATED_RESPONSE:
+        /* Add your code here */
+        break;
 
-  case CDC_SET_COMM_FEATURE:
-    /* Add your code here */
-    break;
+    case CDC_SET_COMM_FEATURE:
+        /* Add your code here */
+        break;
 
-  case CDC_GET_COMM_FEATURE:
-    /* Add your code here */
-    break;
+    case CDC_GET_COMM_FEATURE:
+        /* Add your code here */
+        break;
 
-  case CDC_CLEAR_COMM_FEATURE:
-    /* Add your code here */
-    break;
+    case CDC_CLEAR_COMM_FEATURE:
+        /* Add your code here */
+        break;
 
-  case CDC_SET_LINE_CODING:
-    LineCoding.bitrate = (uint32_t) (pbuf[0] | (pbuf[1] << 8) |
-                                     (pbuf[2] << 16) | (pbuf[3] << 24));
-    LineCoding.format = pbuf[4];
-    LineCoding.paritytype = pbuf[5];
-    LineCoding.datatype = pbuf[6];
+    case CDC_SET_LINE_CODING:
+        LineCoding.bitrate = (uint32_t)(pbuf[0] | (pbuf[1] << 8) |
+            (pbuf[2] << 16) | (pbuf[3] << 24));
+        LineCoding.format = pbuf[4];
+        LineCoding.paritytype = pbuf[5];
+        LineCoding.datatype = pbuf[6];
 
-    break;
+        break;
 
-  case CDC_GET_LINE_CODING:
-    pbuf[0] = (uint8_t) (LineCoding.bitrate);
-    pbuf[1] = (uint8_t) (LineCoding.bitrate >> 8);
-    pbuf[2] = (uint8_t) (LineCoding.bitrate >> 16);
-    pbuf[3] = (uint8_t) (LineCoding.bitrate >> 24);
-    pbuf[4] = LineCoding.format;
-    pbuf[5] = LineCoding.paritytype;
-    pbuf[6] = LineCoding.datatype;
-    break;
+    case CDC_GET_LINE_CODING:
+        pbuf[0] = (uint8_t)(LineCoding.bitrate);
+        pbuf[1] = (uint8_t)(LineCoding.bitrate >> 8);
+        pbuf[2] = (uint8_t)(LineCoding.bitrate >> 16);
+        pbuf[3] = (uint8_t)(LineCoding.bitrate >> 24);
+        pbuf[4] = LineCoding.format;
+        pbuf[5] = LineCoding.paritytype;
+        pbuf[6] = LineCoding.datatype;
+        break;
 
-  case CDC_SET_CONTROL_LINE_STATE:
-    /* Add your code here */
-    break;
+    case CDC_SET_CONTROL_LINE_STATE:
+        /* Add your code here */
+        break;
 
-  case CDC_SEND_BREAK:
-    /* Add your code here */
-    break;
+    case CDC_SEND_BREAK:
+        /* Add your code here */
+        break;
 
-  default:
-    break;
-  }
+    default:
+        break;
+    }
 
-  return (USBD_OK);
+    return (USBD_OK);
 }
 
 /**
   * @brief  CDC_Itf_DataRx
-  *         Data received over USB OUT endpoint are sent over CDC interface 
+  *         Data received over USB OUT endpoint are sent over CDC interface
   *         through this function.
   * @param  Buf: Buffer of data to be transmitted
   * @param  Len: Number of data received (in bytes)
@@ -187,9 +187,9 @@ static int8_t CDC_Itf_Receive(uint8_t *Buf, uint32_t *Len)
         _CDC::in_buffer.Append(*Buf);
         Buf++;
     }
-    
+
     extern USBD_HandleTypeDef USBD_Device;
-    
+
     USBD_CDC_ReceivePacket(&USBD_Device);
 
     return (USBD_OK);
@@ -209,7 +209,7 @@ static int8_t CDC_Itf_Receive(uint8_t *Buf, uint32_t *Len)
   */
 static int8_t CDC_Itf_TransmitCplt(uint8_t * /*Buf*/, uint32_t * /*Len*/, uint8_t /*epnum*/)
 {
-  return (0);
+    return (0);
 }
 
 /**
@@ -224,12 +224,12 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef * /*huart*/)
 
 void _CDC::GetData(BufferOSDP &out_buffer)
 {
-        while (!in_buffer.IsEmpty())
-        {
-            char symbol = (char)in_buffer.Pop();
+    while (!in_buffer.IsEmpty())
+    {
+        char symbol = (char)in_buffer.Pop();
 
-            symbol = (char)std::toupper(symbol);
+        symbol = (char)std::toupper(symbol);
 
-            out_buffer.Append(symbol);
-        }
+        out_buffer.Append(symbol);
+    }
 }
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 927 ;
-const char *DATE_BUILD = "2026-01-20 10:31:22" ;
+unsigned int VERSION_BUILD = 928 ;
+const char *DATE_BUILD = "2026-01-20 10:32:07" ;
