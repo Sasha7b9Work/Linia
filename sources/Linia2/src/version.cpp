d875282eda27aabdcca14fa9815f62ddaeea06fb
Sasha7b9Work <Phuntik-it@tut.by>
2026-01-23 10:46:27 +0300

    Попытка улучшить подключение к COM-порту

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 933 ;
-const char *DATE_BUILD = "2026-01-23 10:32:15" ;
+unsigned int VERSION_BUILD = 934 ;
+const char *DATE_BUILD = "2026-01-23 10:46:27" ;
