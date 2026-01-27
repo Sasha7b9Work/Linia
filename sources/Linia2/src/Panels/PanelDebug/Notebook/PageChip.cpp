5a983b3caa339e2d6e52147d1d7a92f46dd164a1
Sasha7b9Work <Phuntik-it@tut.by>
2026-01-27 12:32:20 +0300

    Начинаем забивать регистр канала базы

--- sources/Linia2/src/Panels/PanelDebug/Notebook/PageChip.cpp
+++ sources/Linia2/src/Panels/PanelDebug/Notebook/PageChip.cpp
@@ -60,25 +60,31 @@ void PageChip::FillRegisterBS(RegFPGA *reg)
     std::vector<StructDescription> desc3; //-V827
 
     std::vector<StructDescription::CommandStruct> commandsLimitCurrent; //-V827 //-V826
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b000, "200 нA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b001, "2 мкA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b010, "20 мкA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b011, "200 мкA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b100, "2 мA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b101, "20 мA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b110, "200 мA" });
-    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b111, "2 A" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b000, "20 нA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b001, "200 нA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b010, "2 мкA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b011, "20 мкA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b100, "200 мкA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b101, "2 мA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b110, "20 мA" });
+    commandsLimitCurrent.emplace_back(StructDescription::CommandStruct{ 0b111, "200 мA" });
     desc3.emplace_back(StructDescription{ 0, 3, "ппт", "Предел по току", { true, commandsLimitCurrent } });
 
-    std::vector<StructDescription::CommandStruct> commandsLimit20A; //-V826
-    commandsLimit20A.emplace_back(StructDescription::CommandStruct{ 0, "Выключено" });
-    commandsLimit20A.emplace_back(StructDescription::CommandStruct{ 1, "Включено" });
-    desc3.emplace_back(StructDescription{ 3, 1, "п", "Предел 20А", { true, commandsLimit20A } });
+    std::vector<StructDescription::CommandStruct> commandsLimit2A; //-V826
+    commandsLimit2A.emplace_back(StructDescription::CommandStruct{ 0, "Выключено" });
+    commandsLimit2A.emplace_back(StructDescription::CommandStruct{ 1, "Включено" });
+    desc3.emplace_back(StructDescription{ 3, 1, "п", "Предел 2А", { true, commandsLimit2A } });
 
-    std::vector<StructDescription::CommandStruct> commandsUc; //-V826
-    commandsUc.emplace_back(StructDescription::CommandStruct{ 0, "Подключение " });
-    commandsUc.emplace_back(StructDescription::CommandStruct{ 1, "Отключение " });
-    desc3.emplace_back(StructDescription{ 4, 1, "U", "Uc", { true, commandsUc } });
+    std::vector<StructDescription::CommandStruct> commandsLimit10A; //-V826
+    commandsLimit10A.emplace_back(StructDescription::CommandStruct{ 0, "Выключено" });
+    commandsLimit10A.emplace_back(StructDescription::CommandStruct{ 1, "Включено" });
+    desc3.emplace_back(StructDescription{ 4, 1, "п", "Предел 10A", { true, commandsLimit10A } });
+
+    std::vector<StructDescription::CommandStruct> commandsOS_OPA; //-V826
+    commandsOS_OPA.emplace_back(StructDescription::CommandStruct{ 0, "В режиме U" });
+    commandsOS_OPA.emplace_back(StructDescription::CommandStruct{ 1, "В режиме I" });
+    desc3.emplace_back(StructDescription{ 5, 1, "о", "Подключение ОС вых ОУ", { true, commandsOS_OPA } });
+
 
     reg->SetDescriptionBits(0, desc3);
 }
