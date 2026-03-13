9a69b2c1fc6b9f3089022eda37d38f7cc2e0ffad
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-13 17:10:23 +0300

    refact

--- sources/Linia2/src/Panels/PanelMenu/DialogGraph.cpp
+++ sources/Linia2/src/Panels/PanelMenu/DialogGraph.cpp
@@ -4,23 +4,14 @@
 
 
 DialogGraph *DialogGraph::self = nullptr;
-DialogGraphColor *DialogGraphColor::self = nullptr;
 
 
 const pchar DialogGraph::BTN_SIZE_POINT = "Размер точки";
 const pchar DialogGraph::BTN_STYLE_CURVE = "Стиль кривой";
-const pchar DialogGraph::BTN_COLOR = "Цвет";
 const pchar DialogGraph::BTN_MARKERS = "Маркеры";
 const pchar DialogGraph::BTN_SCALE = "Шкала";
 const pchar DialogGraph::BTN_SAVE = "Сохранить график в архиве";
 const pchar DialogGraph::BTN_RESET = "Сброс графика Ref";
-
-const pchar DialogGraphColor::BTN_GRID = "Сетки";
-const pchar DialogGraphColor::BTN_FONT = "Шрифта";
-const pchar DialogGraphColor::BTN_CURVE = "Кривой";
-const pchar DialogGraphColor::BTN_LINK = "Ссылки";
-const pchar DialogGraphColor::BTN_SECANT = "Секущей";
-
 
 DialogGraph::DialogGraph() :
     MenuDialog("График", 200, { },
@@ -29,10 +20,6 @@ DialogGraph::DialogGraph() :
         },
         BTN_STYLE_CURVE, []()
         {
-        },
-        BTN_COLOR, []()
-        {
-            DialogGraphColor().ShowOnWindow(DialogGraph::self->FindButton(BTN_COLOR));
         },
         BTN_MARKERS, []()
         {
@@ -46,77 +33,4 @@ DialogGraph::DialogGraph() :
     )
 {
     self = this;
-}
-
-
-DialogGraphColor::DialogGraphColor() :
-    MenuDialog("Цвет", 125, { },
-        BTN_GRID, []()
-        {
-            wxColour color;
-
-            if (SelectColor("сетки", color))
-            {
-
-            }
-        },
-        BTN_FONT, []()
-        {
-            wxColour color;
-
-            if (SelectColor("шрифта", color))
-            {
-
-            }
-        },
-        BTN_CURVE, []()
-        {
-            wxColour color;
-
-            if (SelectColor("кривой", color))
-            {
-
-            }
-        },
-        BTN_LINK, []()
-        {
-            wxColour color;
-
-            if (SelectColor("ссылки", color))
-            {
-
-            }
-        },
-        BTN_SECANT, []()
-        {
-            wxColour color;
-
-            if (SelectColor("секущей", color))
-            {
-
-            }
-        }
-    )
-{
-    self = this;
-}
-
-
-bool DialogGraphColor::SelectColor(const wxString &title, wxColour &color)
-{
-    wxColourData colourData;
-    colourData.SetChooseFull(true);
-
-    wxColourDialog dialog(DialogGraphColor::self, &colourData);
-
-    dialog.SetTitle(wxString("Цвет ") + title);
-
-    if (dialog.ShowModal() == wxID_OK)
-    {
-        color = dialog.GetColourData().GetColour();
-
-        return true;
-    }
-
-    return false;
 }
--- sources/Linia2/src/Panels/PanelMenu/DialogGraph.h
+++ sources/Linia2/src/Panels/PanelMenu/DialogGraph.h
@@ -15,29 +15,8 @@ private:
 
     static const pchar BTN_SIZE_POINT;
     static const pchar BTN_STYLE_CURVE;
-    static const pchar BTN_COLOR;
     static const pchar BTN_MARKERS;
     static const pchar BTN_SCALE;
     static const pchar BTN_SAVE;
     static const pchar BTN_RESET;
-};
-
-
-class DialogGraphColor : public MenuDialog
-{
-public:
-
-    DialogGraphColor();
-
-    static DialogGraphColor *self;
-
-    static bool SelectColor(const wxString &title, wxColour &);
-
-private:
-
-    static const pchar BTN_GRID;
-    static const pchar BTN_FONT;
-    static const pchar BTN_CURVE;
-    static const pchar BTN_LINK;
-    static const pchar BTN_SECANT;
 };
