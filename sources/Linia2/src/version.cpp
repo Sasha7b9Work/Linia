eb3062170c8df4a4684d48f839b3e06183a7d20a
DESKTOP-BTC3BJ6\Sasha <Sasha7b9@tut.by>
2026-03-15 15:18:54 +0300

    *

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1126 ;
-const char *DATE_BUILD = "2026-03-15 15:15:58" ;
+unsigned int VERSION_BUILD = 1127 ;
+const char *DATE_BUILD = "2026-03-15 15:18:53" ;
--- sources/scripts/vs_win/edit.bat
+++ sources/scripts/vs_win/edit.bat
@@ -1,3 +1,3 @@
 @echo on
 
-start ..\..\generated\Linia\Linia2-GUI.sln
+start ..\..\generated\Linia\Linia2-GUI.slnx
