3abc260cb351e7cd245cad0cd8ab3aeac0cb8ba4
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-16 09:54:28 +0300

    *

--- sources/Linia2/src/Panels/WindowCalculation/WindowCalculation.cpp
+++ sources/Linia2/src/Panels/WindowCalculation/WindowCalculation.cpp
@@ -1,15 +1,12 @@
 ﻿// 2026/03/16 09:12:56 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Panels/WindowCalculation/WindowCalculation.h"
-
+#include "Settings/Settings.h"
 
 
 WindowCalculation::WindowCalculation(wxFrame *parent) :
     wxFrame(parent, wxID_ANY, "Расчёт", wxDefaultPosition, wxSize(300, 200), wxDEFAULT_FRAME_STYLE | wxFRAME_FLOAT_ON_PARENT)
 {
-    // Стиль wxFRAME_FLOAT_ON_PARENT гарантирует, что окно будет поверх родителя
-
-    // Создаем кнопку для взаимодействия с главным окном
     wxPanel *panel = new wxPanel(this);
     wxButton *btn = new wxButton(panel, wxID_ANY,
         "Вызвать функцию главного окна",
@@ -17,17 +14,16 @@ WindowCalculation::WindowCalculation(wxFrame *parent) :
 
     btn->Bind(wxEVT_BUTTON, [this, parent](wxCommandEvent &)
         {
-            if (parent)
+            if (GetParent())
             {
-                parent->SetTitle("Главное окно - функция вызвана из дочернего");
-                // Или вызвать любой другой метод главного окна
-                // parent->SomeMethod();
+                ((wxFrame *)GetParent())->SetTitle("Главное окно - функция вызвана из дочернего");
+
+                SET::GUI::color_background.Set((*wxWHITE).GetRGB());
             }
         });
 
     // Кнопка закрытия
-    wxButton *closeBtn = new wxButton(panel, wxID_ANY,
-        "Закрыть", wxPoint(20, 70), wxSize(250, 30));
+    wxButton *closeBtn = new wxButton(panel, wxID_ANY, "Закрыть", wxPoint(20, 70), wxSize(250, 30));
 
     closeBtn->Bind(wxEVT_BUTTON, [this](wxCommandEvent &)
         {
