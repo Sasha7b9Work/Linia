7ad43bde4d72a28589a25fd4221deade052f8e7c
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-17 12:17:16 +0300

    *

--- sources/Linia2/src/Controls/DraggedWindow.cpp
+++ sources/Linia2/src/Controls/DraggedWindow.cpp
@@ -11,6 +11,9 @@ DraggedWindow::DraggedWindow(const wxString &_title, const wxSize &_size)
         wxFRAME_FLOAT_ON_PARENT | wxSTAY_ON_TOP),
     title(_title)
 {
+    SetSizeHints(_size);
+    Fit();
+
     wxSize size = GetClientSize();
 
     main_panel = new wxPanel(this, wxID_ANY, { 0, 0 }, size, wxEXPAND);
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1226 ;
-const char *DATE_BUILD = "2026-03-17 12:14:35" ;
+unsigned int VERSION_BUILD = 1227 ;
+const char *DATE_BUILD = "2026-03-17 12:17:16" ;
