4c5b6c1bf9cb7dc3b921173428e6c3c2b95687fa
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-18 10:28:10 +0300

    *

--- sources/Linia2/src/Display/MenuDisplay.cpp
+++ sources/Linia2/src/Display/MenuDisplay.cpp
@@ -27,8 +27,8 @@ MenuDisplay::MenuDisplay() : wxMenu()
 
     AppendSeparator();
 
-    Bind(wxEVT_MENU, &MenuDisplay::OnSaveGraphic, this, (Append(wxID_ANY, "Сохранить график"))->GetId());
-    Bind(wxEVT_MENU, &MenuDisplay::OnResetGraphic, this, (Append(wxID_ANY, "Сбросить график"))->GetId());
+//    Bind(wxEVT_MENU, &MenuDisplay::OnSaveGraphic, this, (Append(wxID_ANY, "Сохранить график"))->GetId());
+//    Bind(wxEVT_MENU, &MenuDisplay::OnResetGraphic, this, (Append(wxID_ANY, "Сбросить график"))->GetId());
 }
 
 
