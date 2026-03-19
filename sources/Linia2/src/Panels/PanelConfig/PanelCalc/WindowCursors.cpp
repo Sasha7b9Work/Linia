41870e85f749f72bca0f3a50f430b9402bce99c4
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-19 08:23:41 +0300

    refact

--- sources/Linia2/src/Panels/PanelConfig/PanelCalc/WindowCursors.cpp
+++ sources/Linia2/src/Panels/PanelConfig/PanelCalc/WindowCursors.cpp
@@ -9,7 +9,7 @@
 WindowCursors *TheWindowCursors = nullptr;
 
 
-WindowCursors::WindowCursors() : DraggedDialog("Курсоры", {200, 300})
+WindowCursors::WindowCursors() : DraggedDialog(L"Курсоры", {200, 300})
 {
     int x = 10;
     int y = 22;
