caa84c561f3482c407b1283b89297bf4c1c2f8f2
Sasha7b9 <Sasha7b9@tut.by>
2026-03-25 22:31:20 +0300

    warnings PVS-Studio

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1398 ;
-const char *DATE_BUILD = "2026-03-25 22:09:33" ;
+unsigned int VERSION_BUILD = 1399 ;
+const char *DATE_BUILD = "2026-03-25 22:31:20" ;
