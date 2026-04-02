dfa724728d501b1d0183abf5f36b067b697e20a2
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-02 14:59:01 +0300

    *

--- sources/Linia2/src/Panels/PanelDebug/Notebook/Register.cpp
+++ sources/Linia2/src/Panels/PanelDebug/Notebook/Register.cpp
@@ -550,11 +550,11 @@ RegDAC::RegDAC(wxWindow *parent, pchar _title, Chip *_chip, const wxString &_fun
 
     knob = new KnobWidget(painter, 0, 100, 50, { painter->GetSize().x - d - 70, d });
 
-//    knob->Bind(wxEVT_SLIDER, &RegDAC::OnEventKnob, this);
+    knob->Bind(wxEVT_SLIDER, &RegDAC::OnEventKnob, this);
 
     slider = new SliderInt(painter, { painter->BitX(chip->BitDepth() - 5, chip->BitDepth()), 75 }, (chip->BitDepth() - 4) * 20, 0, 100, "");
 
-//    slider->Bind(wxEVT_SLIDER, &RegDAC::OnEventSlider, this);
+    slider->Bind(wxEVT_SLIDER, &RegDAC::OnEventSlider, this);
 }
 
 
@@ -598,24 +598,20 @@ void RegDAC::OnEventSlider(wxCommandEvent &event)
 {
     if (event.GetId() == slider->GetId())
     {
-//        OnEventChangedControl(event.GetInt());
+        OnEventChangedControl(event.GetInt());
     }
 
-//    event.Skip();
+    event.Skip();
 }
 
 
 void RegDAC::OnEventChangedControl(int value)
 {
-    (void)value;
+    int max_value = (1 << NumBitsValue()) - 1;
 
-//    int max_value = (1 << NumBitsValue()) - 1;
-//    (void)max_value;
-//
-//    uint new_value = (uint)((float)max_value * (float)value / 100.0f + 0.5f);
-//    (void)new_value;
+    uint new_value = (uint)((float)max_value * (float)value / 100.0f + 0.5f);
 
-//    SetValueDAC(new_value);
+    SetValueDAC(new_value);
 }
 
 
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1488 ;
-const char *DATE_BUILD = "2026-04-02 14:54:53" ;
+unsigned int VERSION_BUILD = 1489 ;
+const char *DATE_BUILD = "2026-04-02 14:59:01" ;
