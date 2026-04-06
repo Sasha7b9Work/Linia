dd35f4fc0b5d7e080a7cce156a90fc74465a05b4
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-06 14:26:04 +0300

    *

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1534 ;
-const char *DATE_BUILD = "2026-04-06 14:23:07" ;
+unsigned int VERSION_BUILD = 1535 ;
+const char *DATE_BUILD = "2026-04-06 14:26:04" ;
--- sources/scripts/lin/build_run.sh
+++ sources/scripts/lin/build_run.sh
@@ -5,6 +5,7 @@
 ./build.sh
 kill -9 $(pidof Linia2)
 
+export DISPLAY=:0
 /home/mnipi/Desktop/Linia.desktop
 
 #./start_x.sh
