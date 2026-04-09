f2c19d4c7f1a774d937a0b77896c3467156f6a52
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-09 09:04:27 +0300

    refact

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1562 ;
-const char *DATE_BUILD = "2026-04-09 09:02:40" ;
+unsigned int VERSION_BUILD = 1563 ;
+const char *DATE_BUILD = "2026-04-09 09:04:27" ;
