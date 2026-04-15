0fc9f02de0bd87526a4e24e0df59b226a0fc43ba
Sasha7b9Work <Phuntik-it@tut.by>
2026-04-15 09:02:46 +0300

    Анимирована одиночная засылка

--- sources/Linia2/src/Panels/PanelDebug/Notebook/Register.cpp
+++ sources/Linia2/src/Panels/PanelDebug/Notebook/Register.cpp
@@ -255,10 +255,12 @@ void Register::OnEventToggleButton(wxCommandEvent &event)
         if (event.GetInt())
         {
             timerAutoSend.Start(1000);
+            painter->animation->RunPeriodic();
         }
         else
         {
             timerAutoSend.Stop();
+            painter->animation->Stop();
         }
 
         WriteValue();
@@ -275,6 +277,8 @@ void Register::OnEventButton(wxCommandEvent &event)
     if (id == btnSend->GetId())
     {
         WriteValue();
+
+        painter->animation->RunOnce();
     }
 }
 
@@ -304,8 +308,6 @@ void Register::SetActiveAcross(bool active, wxWindow *_wnd)
     }
 
     TheNotebookDebug->EnableSwitching(active);
-
-    painter->EnableAutoSendAnimation(!active);
 }
 
 
