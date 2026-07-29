e87ac3f23289c328026e69def628e6123ec214fe
Sasha7b9Work <Phuntik-it@tut.by>
2026-07-29 11:35:02 +0300

    Теперь подключился rapidJSON через vcpkg

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1876 ;
-const char *DATE_BUILD = "2026-07-29 11:28:40" ;
+unsigned int VERSION_BUILD = 1877 ;
+const char *DATE_BUILD = "2026-07-29 11:35:01" ;
