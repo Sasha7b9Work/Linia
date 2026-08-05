efb8dcd3dcb311b00b25176ea14b2137d297129d
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-05 15:33:57 +0300

    *

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-unsigned int VERSION_BUILD = 1901 ;
-const char *DATE_BUILD = "2026-08-05 15:16:57" ;
+unsigned int VERSION_BUILD = 1902 ;
+const char *DATE_BUILD = "2026-08-05 15:33:57" ;
--- sources/scripts/ThirdParty/lin/install_software.sh
+++ sources/scripts/ThirdParty/lin/install_software.sh
@@ -6,7 +6,7 @@ sudo apt-get install libwebkit2gtk-4.0-dev -y
 sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
 sudo apt-get install libsdl2-2.0-0 -y
 sudo apt-get install libsdl2-dev -y
-apt-get install libnotify-dev -y
+sudo apt-get install libnotify-dev -y
 sudo apt install libglu1-mesa-dev freeglut3-dev libsm-dev libice-dev libx11-dev libxext-dev libxt-dev -y
 # Для сборки deb-пакета
 sudo apt-get install -y dh-make devscripts dpkg debconf debhelper lintian
