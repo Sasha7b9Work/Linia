d0042316ec5434e7a4eccd101d214dc96c346f3e
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-18 09:02:15 +0300

    Добавлены файлы от последнего макетного релиза

---
+++ sources/Linia/src/Communicator/SPI/SPI.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+
+#define SPI_DEVICE "/dev/spidev0.0"
+#define SPI_SPEED  100000
+#define SPI_CHIP   "gpiochip3"
+
+
+// \todo Для SPI нужно использовать 24 SPI0_CS0_M2 и 26 SPI0_CS1_M2. Программные чипселекты слишком долго выполняются
+
+namespace SPI
+{
+    bool IsAvailability();
+
+    void Init();
+    void DeInit();
+
+    bool WriteDynamicDAC(int number_DAC, uint16 value);
+
+    bool IsReady();
+}
---
+++ sources/Linia/src/version.cpp
@@ -0,0 +1,4 @@
+// 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+
+unsigned int VERSION_BUILD = 1576 ;
+const char *DATE_BUILD = "2026-07-20 13:48:19" ;
