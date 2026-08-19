a16db0b911ffe0ed186b34ba7438159067c67218
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-19 16:43:48 +0300

    *

--- doc/_OrangePi/ОС/scripts/usr/local/bin/mount-usb.sh
+++ doc/_OrangePi/ОС/scripts/usr/local/bin/mount-usb.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# Переменные
 MOUNT_POINT="/mnt/usb1"
 USER="mnipi"
-DEVICE="$1"  # Принимаем устройство как параметр
+LOG="/tmp/mount-usb.log"
 
-# Если устройство не передано, выходим
+echo "=== $(date) ===" >> $LOG
+
+# Находим устройство
+DEVICE=$(lsblk -o NAME,TYPE,MOUNTPOINT | grep -E "sd[a-z][0-9]" | grep -v "part" | head -1)
+
+echo "Found device: $DEVICE" >> $LOG
+
 if [ -z "$DEVICE" ]; then
+    echo "No device found" >> $LOG
     exit 1
 fi
 
-# Создаём точку монтирования, если её нет
+DEVICE="/dev/$DEVICE"
+
+# Создаём точку монтирования
 mkdir -p "$MOUNT_POINT"
 
-# Монтируем устройство
-# Для ext4 используем опции uid и gid через командную строку mount
-mount -o rw,noatime "$DEVICE" "$MOUNT_POINT"
+# Монтируем
+echo "Mounting $DEVICE to $MOUNT_POINT" >> $LOG
+mount -o rw,noatime "$DEVICE" "$MOUNT_POINT" 2>> $LOG
 
-# После монтирования меняем владельца папки
-chown -R "$USER":"$USER" "$MOUNT_POINT"
+# Меняем владельца
+chown -R "$USER":"$USER" "$MOUNT_POINT" 2>> $LOG
+
+echo "Done" >> $LOG
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 1949 ;
-const char *DATE_BUILD = "2026-08-19 16:41:12" ;
+unsigned int VERSION_BUILD = 1950 ;
+const char *DATE_BUILD = "2026-08-19 16:43:48" ;
