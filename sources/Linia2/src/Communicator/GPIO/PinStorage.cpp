ffb282dbcf75805d792e5505f5440aa187d91b32
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-20 08:46:08 +0300

    *

--- sources/Linia2/src/Communicator/GPIO/PinStorage.cpp
+++ sources/Linia2/src/Communicator/GPIO/PinStorage.cpp
@@ -1,6 +1,7 @@
 // 2026/07/23 16:23:32 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 #include "defines.h"
 #include "Communicator/GPIO/PinStorage.h"
+#include <cstring>
 
 
 PinInfo PinStorage::GetPinInfo(Pin::E pin_e, bool is_input)
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 1953 ;
-const char *DATE_BUILD = "2026-08-20 08:43:49" ;
+unsigned int VERSION_BUILD = 1954 ;
+const char *DATE_BUILD = "2026-08-20 08:46:08" ;
