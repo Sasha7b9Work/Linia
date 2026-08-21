75f2e0abf8f7ca630e17ae8010465c3deb26c3ba
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-21 15:02:51 +0300

    *

--- sources/scripts/lin/install_soft.sh
+++ sources/scripts/lin/install_soft.sh
@@ -6,3 +6,11 @@ sudo apt-get -y upgrade
 sudo apt-get -y install nginx htop openssh-server openssh-client libssh-dev iptables-persistent mc links
 sudo apt-get -y install cmake gcc make python3 python3-pip python3-venv build-essential gdb
 sudo apt-get -y install libssl-dev libx11-dev libxrandr-dev mesa-common-dev libgl1-mesa-dev libasound2-dev
+
+ARCH=$(uname -m)
+if [[ "$ARCH" == "aarch64" ]] || [[ "$ARCH" == "armv7l" ]] || [[ "$ARCH" == "arm64" ]]; then
+    echo "ARM64/ARM архитектура обнаружена ($ARCH). Устанавливаем libgpiod..."
+    sudo apt-get install libgpiod-dev gpiod -y
+else
+    echo "Архитектура $ARCH не является ARM. Пропускаем установку libgpiod."
+fi
