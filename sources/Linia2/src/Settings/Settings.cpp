8eadfd1cbf02e395bcb80172568f776ce4294b55
Sasha7b9Work <Phuntik-it@tut.by>
2026-08-28 16:26:50 +0300

    refact

--- sources/Linia2/src/Settings/Settings.cpp
+++ sources/Linia2/src/Settings/Settings.cpp
@@ -28,25 +28,25 @@ namespace SET
 
     void Init()
     {
-#define REGISTER(type, name, key, def) \
-            auto* name##_ptr = new Value<type>(key, def); \
-            RegisterValue(#name, new ValueWrapper<type>(name##_ptr));
+#define REGISTER(type, name, def) \
+        auto* name##_ptr = new Value<type>(#name, def); \
+        RegisterValue(#name, new ValueWrapper<type>(name##_ptr));
 
-        REGISTER(bool, debug_mode, "debug_mode", false);
-        REGISTER(bool, debug_emulate_mode, "debug_emulate_mode", false);
-        REGISTER(wxPoint, gui_pos_console, "gui_pos_console", wxPoint(10, 10));
-        REGISTER(wxPoint, gui_size_console, "gui_size_console", wxPoint(400, 400));
-        REGISTER(bool, gui_maximized_console, "gui_maximized_console", false);
-        REGISTER(int, gui_serial_port_num, "gui_serial_port_num", 0);
-        REGISTER(uint, gui_color_background, "gui_color_background", 0xFFFFFF);
-        REGISTER(uint, gui_color_grid, "gui_color_grid", 0);
-        REGISTER(uint, gui_color_font, "gui_color_font", 0);
-        REGISTER(uint, gui_color_curve, "gui_color_curve", 0);
-        REGISTER(uint, gui_color_link, "gui_color_link", 0);
-        REGISTER(uint, gui_color_secant, "gui_color_secant", 0);
-        REGISTER(int, gui_size_point, "gui_size_point", 2);
-        REGISTER(int, current_panel, "current_panel", 0);
-        REGISTER(uint, period_send, "period_send", 100);
+        REGISTER(bool, debug_mode, false);
+        REGISTER(bool, debug_emulate_mode, false);
+        REGISTER(wxPoint, gui_pos_console, wxPoint(10, 10));
+        REGISTER(wxPoint, gui_size_console, wxPoint(400, 400));
+        REGISTER(bool, gui_maximized_console, false);
+        REGISTER(int, gui_serial_port_num, 0);
+        REGISTER(uint, gui_color_background, 0xFFFFFF);
+        REGISTER(uint, gui_color_grid, 0);
+        REGISTER(uint, gui_color_font, 0);
+        REGISTER(uint, gui_color_curve, 0);
+        REGISTER(uint, gui_color_link, 0);
+        REGISTER(uint, gui_color_secant, 0);
+        REGISTER(int, gui_size_point, 2);
+        REGISTER(int, current_panel, 0);
+        REGISTER(uint, period_send, 100);
 
 #undef REGISTER
     }
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2154 ;
-const char *DATE_BUILD = "2026-08-28 16:24:25" ;
+unsigned int VERSION_BUILD = 2155 ;
+const char *DATE_BUILD = "2026-08-28 16:26:49" ;
