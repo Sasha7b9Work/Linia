b7f0d1cb842396fb4a3e0703336112584485f885
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-01 14:08:29 +0300

    refact

--- sources/Linia2/src/IPPP/Keyboard/Antichatter.cpp
+++ sources/Linia2/src/IPPP/Keyboard/Antichatter.cpp
@@ -9,37 +9,52 @@ bool Antichatter::InOut(bool state, int64 timeMS)
     {
         LOG_ERROR("Invalid time in function %s", __FUNCTION__);
 
+        LOG_WRITE("201");
+
         return prev_state;
     }
+
+    LOG_WRITE("202");
 
     prev_time_input = timeMS;
 
     bool result = prev_state;
 
     {
+        LOG_WRITE("203");
+
         if (prev_time_change == 0)  // Находимся в состоянии ожидания, дребезг не обрабатываем
         {
+            LOG_WRITE("204");
             if (state != prev_state)
             {
+                LOG_WRITE("205");
                 result = state;
 
                 prev_time_change = timeMS;
                 prev_state = state;
 
+                LOG_WRITE("206");
                 funcOnChnage(state);
+                LOG_WRITE("207");
             }
         }
         else                        // Было срабатывание, ждём дребезг
         {
             if (timeMS - prev_time_change >= timeAntichatterMS)
             {
+                LOG_WRITE("208");
                 result = state;
 
                 if (prev_state != state)
                 {
                     prev_time_change = timeMS;
 
+                    LOG_WRITE("208");
+
                     funcOnChnage(state);
+
+                    LOG_WRITE("209");
                 }
                 else
                 {
@@ -50,6 +65,8 @@ bool Antichatter::InOut(bool state, int64 timeMS)
             }
         }
     }
+
+    LOG_WRITE("210");
 
     return result;
 }
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2229 ;
-const char *DATE_BUILD = "2026-09-01 14:06:17" ;
+unsigned int VERSION_BUILD = 2230 ;
+const char *DATE_BUILD = "2026-09-01 14:08:29" ;
