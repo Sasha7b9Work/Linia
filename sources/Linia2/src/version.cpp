da25cd38404fe9d6c979f26dd6064e5488c9fbff
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-03 11:28:47 +0300

    Рисуем тест

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2310 ;
-const char *DATE_BUILD = "2026-09-03 11:27:02" ;
+unsigned int VERSION_BUILD = 2311 ;
+const char *DATE_BUILD = "2026-09-03 11:28:46" ;
