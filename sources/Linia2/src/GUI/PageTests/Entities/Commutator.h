a7d95d1d2a33ced20e8e91031393d5413a420fa2
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-14 16:17:18 +0300

    *

--- sources/Linia2/src/GUI/PageTests/Entities/Commutator.h
+++ sources/Linia2/src/GUI/PageTests/Entities/Commutator.h
@@ -24,7 +24,7 @@ public:
         radius = w / 2;
 
 #ifndef WIN32
-        radius--;
+        radius -= 3;
 #endif
 
         canvas->Bind(wxEVT_PAINT, &Commutator::OnEventPaint, this);
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2506 ;
-const char *DATE_BUILD = "2026-09-14 16:16:24" ;
+unsigned int VERSION_BUILD = 2507 ;
+const char *DATE_BUILD = "2026-09-14 16:17:18" ;
