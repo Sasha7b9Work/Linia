4133a7a0de4fc0f066d1ee78de9d75b06f28eb95
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-14 13:25:15 +0300

    *

--- sources/Linia2/src/GUI/Controls/StaticBox.h
+++ sources/Linia2/src/GUI/Controls/StaticBox.h
@@ -9,7 +9,7 @@ class StaticBox : public wxStaticBox
 {
 public:
     StaticBox(wxWindow *parent, const wxString &title, const wxSize &size = wxDefaultSize) :
-        wxStaticBox(parent, wxID_ANY, title, wxDefaultPosition, size, wxBORDER_SIMPLE)
+        wxStaticBox(parent, wxID_ANY, title, wxDefaultPosition, size, wxBORDER_SUNKEN)
     {
         wxStaticBox::SetName(parent->GetName());
 
