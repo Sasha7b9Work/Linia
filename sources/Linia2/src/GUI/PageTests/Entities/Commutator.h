63b72de8ede4028180de69c71ceb5b50794862b2
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-14 18:00:30 +0300

    *

--- sources/Linia2/src/GUI/PageTests/Entities/Commutator.h
+++ sources/Linia2/src/GUI/PageTests/Entities/Commutator.h
@@ -132,7 +132,7 @@ public:
         btnMode3kV = new Button(canvas, L("3 кВ"), size);
         btnMode3kV->SetPosition({ radius - size.x / 2, 10 });
 
-        txtValue = new StaticText(canvas, "", { 118, 25 }, wxALIGN_CENTER_HORIZONTAL);
+        txtValue = new StaticText(canvas, "", { 118, 20 }, wxALIGN_CENTER_HORIZONTAL);
         txtValue->SetPosition({ radius - txtValue->GetSize().x / 2, 90 });
         wxFont font = txtValue->GetFont();
         font.SetPointSize(25);
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2518 ;
-const char *DATE_BUILD = "2026-09-14 17:59:18" ;
+unsigned int VERSION_BUILD = 2519 ;
+const char *DATE_BUILD = "2026-09-14 18:00:29" ;
