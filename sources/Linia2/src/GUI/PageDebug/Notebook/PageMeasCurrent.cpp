d5be747d0022c9c7977146e230abdabfa4268a98
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-15 10:46:51 +0300

    Размер регистра тока уменьшен до 8 бит

--- sources/Linia2/src/GUI/PageDebug/Notebook/PageMeasCurrent.cpp
+++ sources/Linia2/src/GUI/PageDebug/Notebook/PageMeasCurrent.cpp
@@ -5,7 +5,6 @@
 
 /*
     ADC     AD7691      16 разрядов
-    Управление          16 разрядов
 */
 
 PageMeasCurrent *PageMeasCurrent::self = nullptr;
@@ -20,11 +19,39 @@ PageMeasCurrent::PageMeasCurrent(wxNotebook *notebook) :
 
     std::vector<StructDescription> desc5;
 
-    desc5.emplace_back(StructDescription{ 0, 3, "предел", "Установка предела 200нА/20мкА/2мА/200мА/20А/200А", { true }, });
-    desc5.emplace_back(StructDescription{ 3, 2, "к1", "Установка коэффициента 1/2/4/8", { true },});
-    desc5.emplace_back(StructDescription{ 5, 1, "к2", "Установка коэффициента 1/10" });
-    desc5.emplace_back(StructDescription{ 6, 1, "ё", "Дополнительная ёмкость в режиме DC" });
-    desc5.emplace_back(StructDescription{ 7, 1, "б", "Блокировка сигнала Limit I" });
+    std::vector<StructDescription::CommandStruct> commandKoff;
+    commandKoff.emplace_back(StructDescription::CommandStruct{ 0b00, "x1" });
+    commandKoff.emplace_back(StructDescription::CommandStruct{ 0b01, "x2" });
+    commandKoff.emplace_back(StructDescription::CommandStruct{ 0b10, "x4" });
+    commandKoff.emplace_back(StructDescription::CommandStruct{ 0b11, "x8" });
+    desc5.emplace_back(StructDescription{ 0, 2 , "k", "коэфффициент", { true, commandKoff } });
+
+    std::vector<StructDescription::CommandStruct> commandRange;
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b000, "200 нА" });
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b001, "20 мкА" });
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b010, "2 мА" });
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b011, "200 мА" });
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b100, "20 А" });
+    commandRange.emplace_back(StructDescription::CommandStruct{ 0b101, "200 А" });
+    desc5.emplace_back(StructDescription{ 2, 3, "п", "пределы", { true, commandRange} });
+
+    std::vector<StructDescription::CommandStruct> commandBlock;
+    commandBlock.emplace_back(StructDescription::CommandStruct{ 0b0, "блокировка сигнала OLVI,1 "
+        "о превышении порогового уровня тока в "
+        "режимах \"Коллектор в обрыве\" и \"Стоп\"."
+        "(В шине - сигнал \"unOLVI,1\")" });
+    commandBlock.emplace_back(StructDescription::CommandStruct{ 0b1, "для всех остальных режимов" });
+    desc5.emplace_back(StructDescription{ 5, 1 , "б", "блокировка", { true, commandBlock } });
+
+    std::vector<StructDescription::CommandStruct> commandCapacity;
+    commandCapacity.emplace_back(StructDescription::CommandStruct{ 0b0, "для всех режимов кроме DC" });
+    commandCapacity.emplace_back(StructDescription::CommandStruct{ 0b1, "дополнительная ёмкость в режиме DC" });
+    desc5.emplace_back(StructDescription{ 6, 1, "ё", "дополнительная ёмкость", {true, commandCapacity } });
+
+    std::vector<StructDescription::CommandStruct> commandK;
+    commandK.emplace_back(StructDescription::CommandStruct{ 0b0, "x1" });
+    commandK.emplace_back(StructDescription::CommandStruct{ 0b1, "x10" });
+    desc5.emplace_back(StructDescription{ 7, 1, "к", "коэффициент", { true, commandK } });
 
     reg5->SetDescriptionBits(0, desc5);
 
