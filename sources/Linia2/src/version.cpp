acf199bdde3212ce7e7661c2d34ec4983a8963e5
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-17 14:28:15 +0300

    Исправлены баги с 50+,50-

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2535 ;
-const char *DATE_BUILD = "2026-09-17 14:25:43" ;
+unsigned int VERSION_BUILD = 2536 ;
+const char *DATE_BUILD = "2026-09-17 14:28:15" ;
