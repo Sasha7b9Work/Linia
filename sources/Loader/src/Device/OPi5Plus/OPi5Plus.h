ac77020771ca31a62785a48de0bacebae83cf42b
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-22 10:15:10 +0300

    *

---
+++ sources/Loader/src/Device/OPi5Plus/OPi5Plus.h
@@ -0,0 +1,11 @@
+// 2025/09/22 13:50:12 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
+#pragma once
+
+
+// Orange Pi 5 Plus
+
+
+namespace OPi5Plus
+{
+    void PeriodicTask();
+}
--- sources/Loader/src/main.cpp
+++ sources/Loader/src/main.cpp
@@ -1,3 +1,25 @@
+// 2026/09/22 10:10:06 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
+#include "defines.h"
+#include "Hardware/HAL/HAL.h"
+#include "Device/OPi5Plus/OPi5Plus.h"
+#include "Device/Device.h"
+#include "Hardware/VCP.h"
+
+
 int main()
 {
+    HAL::Init();
+
+    HAL_TIM::Delay(500);
+
+    Device::Init();
+
+    while (true)
+    {
+        OPi5Plus::PeriodicTask();
+
+        VCP::PeriodicTask();
+
+        Device::PeriodicTask();
+    }
 }
