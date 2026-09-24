cd3c853a7239a17fca0025efff425f3ba89c25ab
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-24 15:41:14 +0300

    *

--- sources/Device/src/Device/Device.cpp
+++ sources/Device/src/Device/Device.cpp
@@ -70,5 +70,5 @@ void Device::SendVersion()
 {
     LOG_WRITE("STM32 : Version : %d, Date build : %s", VERSION_BUILD, DATE_BUILD);
 
-    OPi5Plus::SCPI::SendFormat(":INFO:STM32 : VER : %d, DATE : %s", VERSION_BUILD, DATE_BUILD);
+    OPi5Plus::SCPI::SendFormat(":INFO:STM32 : VER  S I O N : %d, DATE : %s", VERSION_BUILD, DATE_BUILD);
 }
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2706 ;
-const char *DATE_BUILD = "2026-09-24 15:41:00" ;
+unsigned int VERSION_BUILD = 2707 ;
+const char *DATE_BUILD = "2026-09-24 15:41:14" ;
