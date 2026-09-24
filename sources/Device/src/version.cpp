f07386851ba0162165cb3c58c3bb0106d838e6f1
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-24 16:14:39 +0300

    *

--- sources/Device/src/version.cpp
+++ sources/Device/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 4 ;
-const char *DATE_BUILD = "2026-09-24 16:10:58" ;
+unsigned int VERSION_BUILD = 5 ;
+const char *DATE_BUILD = "2026-09-24 16:14:39" ;
--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2710 ;
-const char *DATE_BUILD = "2026-09-24 16:10:58" ;
+unsigned int VERSION_BUILD = 2711 ;
+const char *DATE_BUILD = "2026-09-24 16:14:39" ;
