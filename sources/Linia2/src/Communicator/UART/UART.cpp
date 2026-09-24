1e16620f0af7c4cc0e5a904f7be633e58eb904c9
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-24 14:08:22 +0300

    *

--- sources/Linia2/src/Communicator/UART/UART.cpp
+++ sources/Linia2/src/Communicator/UART/UART.cpp
@@ -322,11 +322,6 @@ bool UART::ConfigurePort()
 
 void *UART::ReaderThreadFunc(void *)
 {
-    {
-        struct sched_param param;
-        param.sched_priority = 50;
-        pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
-    }
     uint8 buffer[4096];
     fd_set read_fds;
     struct timeval timeout;
