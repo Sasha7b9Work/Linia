f4d6792b202788d72881961fa9092ef0d9fb7ce5
Sasha7b9Work <Phuntik-it@tut.by>
2026-09-24 09:30:59 +0300

    *

--- sources/Linia2/src/version.cpp
+++ sources/Linia2/src/version.cpp
@@ -1,4 +1,4 @@
 // 2025/10/03 21:36:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@gmail.com
 
-unsigned int VERSION_BUILD = 2648 ;
-const char *DATE_BUILD = "2026-09-24 09:30:31" ;
+unsigned int VERSION_BUILD = 2649 ;
+const char *DATE_BUILD = "2026-09-24 09:30:58" ;
